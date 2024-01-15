2cd3a7856fee100b9d1b5022432cd26fc264fa84
HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2024-01-15 14:32:24 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // scanner 선언
+        Scanner sc = new Scanner(System.in);
+
+        String A = sc.nextLine();
+        System.out.println(A + "??!");
+    }
+}
