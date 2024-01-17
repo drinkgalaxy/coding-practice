c46e7d7e22f41d55755e01e31139e7f7752a4647
HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2024-01-17 23:43:11 +0900

    [Bronze V] Title: 꼬마 정민, Time: 204 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // scanner 선언
+        Scanner sc = new Scanner(System.in);
+
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+
+        System.out.println(A+B+C);
+
+    }
+}
