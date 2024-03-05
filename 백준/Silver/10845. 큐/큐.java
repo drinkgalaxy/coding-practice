14ea97a669e03aadfa9069a6993c7ba9c77c0558
HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2024-03-06 00:40:52 +0900

    [Silver IV] Title: 큐, Time: 140 ms, Memory: 17068 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Queue<Integer> queue = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String S = st.nextToken();
+            if (S.equals("push")) {
+                queue.offer(Integer.parseInt(st.nextToken()));
+            } else if (S.equals("pop")) {
+                if (!queue.isEmpty()) {
+                    sb.append(queue.remove());
+                    sb.append("\n");
+                } else {
+                    sb.append(-1);
+                    sb.append("\n");
+                }
+            } else if (S.equals("size")) {
+                sb.append(queue.size());
+                sb.append("\n");
+            } else if (S.equals("empty")) {
+                if (queue.isEmpty()) {
+                    sb.append(1);
+                    sb.append("\n");
+                } else {
+                    sb.append(0);
+                    sb.append("\n");
+                }
+            } else if (S.equals("front")) {
+                if (!queue.isEmpty()) {
+                    sb.append(queue.peek());
+                    sb.append("\n");
+                } else {
+                    sb.append(-1);
+                    sb.append("\n");
+                }
+            } else if (S.equals("back")) {
+                if (!queue.isEmpty()) {
+                    sb.append(((LinkedList<Integer>) queue).peekLast());
+                    sb.append("\n");
+                } else {
+                    sb.append(-1);
+                    sb.append("\n");
+                }
+
+            }
+        }
+        System.out.println(sb);
+    }
+}
