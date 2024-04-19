fd3d4ac94ba712ba24cea15a4e59f027d394307c
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-04-20 01:10:03 +0900

    [Bronze II] Title: 수 정렬하기, Time: 152 ms, Memory: 13696 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,26 +1,25 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
         int[] array = new int[N];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             array[i] = Integer.parseInt(st.nextToken());
         }
-        for (int i = 1; i < array.length; i++) {
-            int temp = array[i]; // 값을 미리 저장
-            int j = i-1; // 자기보다 1칸 작은 수부터 비교를 시작함
-            while(j >= 0 && array[j] > temp) {
-                array[j+1] = array[j];
-                j--; // 선택할 원소를 삽입할 위치를 찾기위해 j--
+        for (int i = 0; i < N-1; i++) {
+            for (int j = 0; j < N-1-i; j++) {
+                if (array[j] > array[j+1]) {
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
             }
-            array[j+1] = temp;
         }
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < N; i++) {
             System.out.println(array[i]);
         }
 
