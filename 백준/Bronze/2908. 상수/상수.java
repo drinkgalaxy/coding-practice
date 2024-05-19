336b63a966d4a92603bb5ac3bdf33de5135bd9d1
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-05-19 16:52:23 +0900

    [Bronze II] Title: 상수, Time: 80 ms, Memory: 11632 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,33 @@
+import java.awt.font.NumericShaper;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static String bigNumber;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        String s1 = st.nextToken();
+        String s2 = st.nextToken();
+        String[] s1Array = s1.split("");
+        String[] s2Array = s2.split("");
+        for (int i = s1Array.length-1; i >= 0; i--) {
+            sb1.append(s1Array[i]);
+        }
+        for (int i = s2Array.length-1; i >= 0; i--) {
+            sb2.append(s2Array[i]);
+        }
+        String number1 = sb1.toString();
+        String number2 = sb2.toString();
+        int n1 = Integer.parseInt(number1);
+        int n2 = Integer.parseInt(number2);
+        if (n1 > n2) {
+            bigNumber = Integer.toString(n1);
+        } else {
+            bigNumber = Integer.toString(n2);
+        }
+        System.out.println(bigNumber);
+    }
+}
