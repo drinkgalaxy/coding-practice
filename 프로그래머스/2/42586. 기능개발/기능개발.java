3e6b788f0c199d116f654b4bf4872b6425e3f444
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-05-20 23:25:16 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,26 +3,34 @@ import java.util.*;
 class Solution {
     static Queue<Integer> queue = new LinkedList<>();
     static int[] takeDay;
-    public int solution(int[] progresses, int[] speeds) {
+    public ArrayList<Integer> solution(int[] progresses, int[] speeds) {
         takeDay = new int[progresses.length];
         for (int i = 0; i < progresses.length; i++) {
-            takeDay[i] = (100-progresses[i])/speeds[i];
-            queue.add(takeDay[i]);
+            if ((100 - progresses[i]) % speeds[i] == 0) {
+                queue.add((100 - progresses[i]) / speeds[i]);
+            } else {
+                queue.add((100 - progresses[i]) / speeds[i] + 1);
+            }
         }
-        ArrayList<Integer> result = Comparison(queue);
-        return result;
+        return Comparison(queue);
     }
     
     static public ArrayList<Integer> Comparison(Queue<Integer> queue) {
         ArrayList<Integer> result = new ArrayList<>();
         while (!queue.isEmpty()) {
-            int count = 1;
             int front = queue.poll();
-            while (!queue.isEmpty() && queue.peek() != null && queue.peek() <= front) {
-                queue.poll();
-                count++;
+            int count = 1;
+            if (queue.peek() != null && queue.peek() <= front) {
+                while (queue.peek() != null && queue.peek() <= front) {
+                    queue.poll();
+                    count++;
+                }
+            } else {
+                result.add(count);
+                continue;
             }
+            result.add(count);
         }
-        result.add(count);
+        return result;
     }
 }
