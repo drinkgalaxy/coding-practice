8156ce5d5cf1d743fd8012c71dd8e6b8ba86ea72
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-05-22 22:23:24 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 29.02 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -7,18 +7,18 @@ public class Solution {
         for (int i = 0; i < arr.length; i++) {
             if (i < arr.length-1) {
                 if (arr[i] != arr[i+1]) {
-                    queue.add(arr[i]);
-                }
-            } else {
+                queue.add(arr[i]);
+           } 
+         } else {
                 queue.add(arr[i]);
             }
-        } 
+        }
         int time = queue.size();
         int[] result = new int[time];
         for (int i = 0; i < time; i++) {
             result[i] = queue.poll();
         }
+
         return result;
     }
-    
 }
