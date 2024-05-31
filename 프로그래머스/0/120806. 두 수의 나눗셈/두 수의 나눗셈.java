813963ca47f44655190ea42831d02f338fddd65d
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-05-31 23:26:57 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -1,10 +1,7 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;
-        Double doubleNum1 = Double.valueOf(num1);
-        Double doubleNum2 = Double.valueOf(num2);
-        Double multi = (doubleNum1 / doubleNum2) * 1000;
-        int result = multi.intValue();
-        return result;
+        double multi =((double) num1 / (double) num2) * 1000;
+        int answer = (int) multi;
+        return answer;
     }
 }
