b117a5a174b0899beb24c592e42bcd45819f8476
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-06-14 16:18:22 +0900

    Create 1470. Shuffle the Array.java

---
+++ LeetCode/1470. Shuffle the Array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+
+        // n 기준 인덱스로 배열을 나누기
+        int[] num1 = new int[n];
+        for (int i = 0; i < n; i++) {
+            num1[i] = nums[i];
+        }
+        int[] num2 = new int[n];
+        for (int i = 0; i < n; i++) {
+            num2[i] = nums[n+i];
+        }
+
+        // 배열을 순서대로 섞기
+        // 홀수면 num1의 값, 짝수면 num2의 값
+        int num1Count = 0;
+        int num2Count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                    nums[i] = num1[num1Count];
+                    num1Count++;
+                } else {
+                    nums[i] = num2[num2Count];
+                    num2Count++;
+                }
+        }
+
+        return nums;
+    }
+}
