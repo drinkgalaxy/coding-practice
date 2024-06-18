f2f56032de5e910b78c6729969678ae303960858
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-06-18 15:49:42 +0900

    Update 1528. Shuffle String.java

--- LeetCode/1528. Shuffle String.java
+++ LeetCode/1528. Shuffle String.java
@@ -1,11 +1,11 @@
 class Solution {
     public String restoreString(String s, int[] indices) {
         // String 배열을 생성하고 indices 인덱스값에 String 순서대로 split해서 넣어주기
-        String[] strArray = new String[indices.length];
         ArrayList<String> strList = new ArrayList<>();
         for (String i : s.split("")) {
             strList.add(i);
         }
+        String[] strArray = new String[indices.length];
         for (int i = 0; i < indices.length; i++) {
             strArray[indices[i]] = strList.get(i);
         }
