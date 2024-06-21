3bd4e142e0e45af42702294c1d2cd4c0ff7b3b4d
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-06-21 12:30:17 +0900

    [level 1] Title: 폰켓몬, Time: 1.70 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,15 +1,23 @@
 import java.util.*;
 
-
 class Solution {
     public int solution(int[] nums) {
-        Set<Integer> phoneketmon = new HashSet<>();
+        // 최대한 많은 폰켓몬 -> set 으로 중복 제거
+        // nums.length 만큼 돌면서 set을 사용해서 저장하고, nums가 없으면 저장후 result++
+        // answer가 nums.length/2 보다 크면 nums.length/2 return.
+        
+        Set<Integer> set = new HashSet<>();
+        int answer = 0;
         for (int i = 0; i < nums.length; i++) {
-            phoneketmon.add(nums[i]);
+            if (!set.contains(nums[i])) {
+                set.add(nums[i]);
+                answer++;
+            } 
         }
-        if (phoneketmon.size() >= nums.length/2) {
+        
+        if (answer >= nums.length/2) {
             return nums.length/2;
         }
-        return phoneketmon.size();
+        return answer;
     }
 }
