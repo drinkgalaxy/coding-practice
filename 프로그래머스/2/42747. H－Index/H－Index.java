77ae2259c34e3354e88a4d54031d742e4786bf11
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-06-21 16:01:20 +0900

    Update H－Index.java

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,19 +1,13 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] citations) {
-        // 논문의 인용 횟수 정렬
+        int answer = 0;
         Arrays.sort(citations);
-        
-        int n = citations.length;
-        
-        // h의 최댓값 찾기
-        for (int i = 0; i < n; i++) {
-            int h = n - i; 
-            if (citations[i] >= h) {
-                return h;
-            }
+        // 정렬된 배열을 순회하면서, 각 논문의 인용 횟수와 해당 인용 횟수보다 많은 논문의 수를 비교합니다.
+    
+        for(int i=0; i<citations.length; i++){
+            int smaller = Math.min(citations[i], citations.length-i);
+            answer = Math.max(answer, smaller);
         }
-        return 0;
+        return answer;
     }
 }
