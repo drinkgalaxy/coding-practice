4bfd10eb957a262757d5bcdee322ad9d403fe43b
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-06-22 22:41:01 +0900

    [level 0] Title: 합성수 찾기, Time: 2.00 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            int count = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (count >= 3) {
+                list.add(i);
+            }
+        }
+        for (Integer i : list) {
+            System.out.print(i+", ");
+        }
+        
+        int answer = list.size();
+        return answer;
+    }
+}
