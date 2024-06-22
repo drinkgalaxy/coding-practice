5598b8431d9bdd7182a2fe4a6b99db2f04232329
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-06-22 22:00:42 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.19 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -2,37 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(String t, String p) {
-        List<String> strList = new ArrayList<>();
-        List<Long> longList = new ArrayList<>();
+        int answer = 0;
         int pLength = p.length();
         long pLong = Long.valueOf(p);
         
-        // t를 String list로 저장
-        for (String str : t.split("")) {
-            strList.add(str);
-        }
-        // String list로부터 값을 p.length 만큼씩 가져와서 더하고 Integer list로 새로 저장
-        for (int i = 0; i <= strList.size() - pLength; i++) {
-            longList.add(Long.valueOf(strJoin(strList, i, pLength)));
-        }
-        
-        int answer = 0;
-        // Integer list랑 p랑 비교 후 작거나 같으면 answer++
-        for (int i = 0; i < longList.size(); i++) {
-            if (longList.get(i) <= pLong) {
+        for (int i = 0; i <= t.length() - pLength; i++) {
+            long tSub = Long.valueOf(t.substring(i, i+pLength));
+            if (tSub <= pLong) {
                 answer++;
             }
         }
-
+        
         return answer;
-    }
-    
-    public static String strJoin(List<String> strList, int index, int repeat) {
-        String newString = "";
-        for (int i = 0; i < repeat; i++) {
-            newString += strList.get(index+i);
-        }
-
-        return newString;
     }
 }
