a6915769de16169241c88f2ff515ea5b431c1208
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-06-28 14:48:36 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -1,11 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int count = 0;
-        for (int i = 0; i <= n; i++) {
+        int answer = 0;
+        for (int i = 2; i <= n; i++) {
             if (i % 2 == 0) {
-                count += i;
+                answer = answer + i;
             }
         }
-        return count;
+        return answer;
     }
 }
