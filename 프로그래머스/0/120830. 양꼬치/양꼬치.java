1db27154e26dbc344d2b5d923846dc2b59e924ba
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-06-28 14:41:50 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,10 +1,9 @@
 class Solution {
     public int solution(int n, int k) {
-        int price = 0;
-        price = (n * 12000) + (k * 2000);
-        if (n / 10 > 0) {
-            price -= (n / 10) * 2000;
+        int service = 0;
+        if (n >= 10) {
+            service = n / 10;
         }
-        return price;
-    }   
+        return (n * 12000) + ((k - service) * 2000);
+    }
 }
