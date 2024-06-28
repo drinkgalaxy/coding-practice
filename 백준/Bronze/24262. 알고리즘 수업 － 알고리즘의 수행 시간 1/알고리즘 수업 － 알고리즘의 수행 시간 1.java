9ed017e3fb1c73e882231264933cc8306ec68405
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-06-28 15:09:28 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 72 ms, Memory: 11472 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 알고리즘의 수행 횟수, 알고리즘의 수행 횟수 다항식의 최고차항의 차수 출력
+        // 문제에서 주어진 알고리즘의 수행 횟수는 1, 최고차항의 차수는 0
+        System.out.println(1);
+        System.out.println(0);
+
+    }
+}
