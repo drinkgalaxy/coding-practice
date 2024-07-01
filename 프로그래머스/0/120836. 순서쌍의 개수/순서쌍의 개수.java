b9ae30cc7aafce5b8ba6e06be0a3c995a3591b62
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-07-01 23:56:20 +0900

    [level 0] Title: 순서쌍의 개수, Time: 7.05 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -2,17 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int n) {
-        Map<Integer, Integer> map = new HashMap<>();
+        int answer = 0;
         for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
                 int value = n / i;
-                map.put(i, value);
-            }
-        }
-        
-        int answer = 0;
-        for (Integer key : map.keySet()) {
-            answer++;
+                answer++;
+            }   
         }
         return answer;
     }
