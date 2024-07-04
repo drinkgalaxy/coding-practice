b8748233f536cc674ed64094a7ca2c8e5cc479a7
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-07-04 18:23:47 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -1,6 +1,7 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        // number가 너무 커지는 걸 방지하기 위해 1부터 시작, number가 n이 커지기 전까지 진행
         for (int i = 1; i <= n; i++) {
             int number = getFactorial(i);
             if (number > n) {
