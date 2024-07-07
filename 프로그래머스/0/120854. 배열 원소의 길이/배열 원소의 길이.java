bf72c62a8a17cdc79a644a1d93082a18429dc3cd
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-07-07 16:42:46 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.03 ms, Memory: 67.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        int index = 0;
+        for (String s : strlist) {
+            answer[index] = s.length();
+            index++;
+        }
+        return answer;
+    }
+}
