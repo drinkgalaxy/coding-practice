c003e53c87bfc4b45d1f89b49117ef5be293cc88
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-07-14 15:03:29 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 65.1 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -3,16 +3,12 @@ import java.util.*;
 class Solution {
     public int solution(String[] s1, String[] s2) {
         int answer = 0;
-        Map<String, Integer> map = new HashMap<>();
-        for (String s : s1) {
-            map.put(s, map.getOrDefault(s, 0) + 1);
-        }
-        for (String s : s2) {
-            map.put(s, map.getOrDefault(s, 0) + 1);
-        }
-        for (String s : map.keySet()) {
-            if (map.get(s) != 1) {
-                answer += 1;
+        for (String str1 : s1) {
+            for (String str2 : s2) {
+                if (str1.equals(str2)) {
+                    answer++;
+                    break;
+                }
             }
         }
         return answer;
