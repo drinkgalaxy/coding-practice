2de7536969f18d878395678b6ff016752c7bf46f
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-07-14 13:49:39 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.java
+++ 프로그래머스/0/120898. 편지/편지.java
@@ -1,9 +1,5 @@
 class Solution {
     public int solution(String message) {
-        int count = 0;
-        for (String s : message.split("")) {
-            count += 2;
-        }
-        return count;
+        return message.length() * 2;
     }
 }
