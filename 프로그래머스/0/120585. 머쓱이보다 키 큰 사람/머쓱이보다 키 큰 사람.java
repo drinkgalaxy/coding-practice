ca495ebd64b8d6aae81943b5653a177c0d60dd64
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-07-17 11:43:03 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.06 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(int[] array, int height) {
-        int count = 0;
-        for (int i = 0; i < array.length; i++)
-            if (array[i] > height) {
-                count ++;
+        int answer = 0;
+        for (Integer i : array) {
+            if (i > height) {
+                answer++;
             }
-            return count;
+        }
+        return answer;
     }
 }
