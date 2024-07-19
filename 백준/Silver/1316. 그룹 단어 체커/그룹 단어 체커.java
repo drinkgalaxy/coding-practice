d3877167d6523347bef42b1266770f07f76c50d2
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-07-19 20:09:03 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 76 ms, Memory: 11612 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,28 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashSet;
 
+import java.io.*;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         HashSet<Character> set;
-        int n = Integer.parseInt(br.readLine());
+
+        int N = Integer.parseInt(br.readLine());
         int count = 0;
-        for (int i = 0; i < n; i++) {
+
+        for (int i = 0; i < N; i++) {
             String word = br.readLine();
             set = new HashSet<>();
             boolean isGroupWord = true;
             char preChar = 0;
             for (int j = 0; j < word.length(); j++) {
                 char currentChar = word.charAt(j);
-                if (currentChar != preChar) {
+                if (preChar != currentChar) {
                     if (set.contains(currentChar)) {
                         isGroupWord = false;
                         break;
+                    } else {
+                        set.add(currentChar);
                     }
-                    set.add(currentChar);
                 }
                 preChar = currentChar;
             }
@@ -30,7 +31,6 @@ public class Main {
                 count++;
             }
         }
-
         System.out.println(count);
     }
 }
