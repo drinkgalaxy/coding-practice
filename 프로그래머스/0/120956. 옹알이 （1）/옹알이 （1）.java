700168a5fc96e0cb1d303397e1cb8c3e938d11b1
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-07-23 13:55:27 +0900

    [level 0] Title: 옹알이 (1), Time: 5.73 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        String[] word = {"aya", "ye", "woo", "ma"};
+        for (int i = 0; i < babbling.length; i++) {
+            for (int j = 0; j < word.length; j++) {
+                babbling[i] = babbling[i].replace(word[j], " ");
+            }
+        }
+        
+        for (String s : babbling) {
+            System.out.println("s = "+ s);
+            if (s.isBlank()) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
