5443057e38cd305619b0d3bc1205f09c5dc63f0e
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-07-25 10:18:55 +0900

    [level 0] Title: 종이 자르기, Time: 0.03 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -1,9 +1,5 @@
 class Solution {
     public int solution(int M, int N) {
-        if (M == 0 || N == 0) {
-            return 0;
-        }
-        
         return (M - 1) + (M * (N - 1));
     }
 }
