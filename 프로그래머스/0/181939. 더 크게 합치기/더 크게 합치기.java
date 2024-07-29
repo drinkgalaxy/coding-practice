066a57d68f9b4c337357fd7dd7799b64892056ab
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-07-29 16:33:54 +0900

    [level 0] Title: 더 크게 합치기, Time: 2.57 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,15 +1,9 @@
 class Solution {
     public int solution(int a, int b) {
-        String ab = String.valueOf(a) + String.valueOf(b);
-        String ba = String.valueOf(b) + String.valueOf(a);
 
-        int abValue = Integer.parseInt(ab);
-        int baValue = Integer.parseInt(ba);
+        String a_ = String.valueOf(a);
+        String b_ = String.valueOf(b);
         
-        if (abValue >= baValue) {
-            return abValue;
-        } else {
-            return baValue;
-        }
+        return Math.max(Integer.valueOf(a_ + b_), Integer.valueOf(b_ + a_));
     }
 }
