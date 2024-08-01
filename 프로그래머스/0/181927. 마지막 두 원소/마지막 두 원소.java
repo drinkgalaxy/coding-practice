5e5ea2ff04872ac51f097afe934e1e30de97902b
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-08-01 10:23:14 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 65.2 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -2,14 +2,21 @@ class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length + 1];
         
+        int last = num_list[num_list.length - 1];
+        int before = num_list[num_list.length - 2];
+        
         for (int i = 0; i < num_list.length; i++) {
-            answer[i] = num_list[i];
-            if (num_list[num_list.length-1] > num_list[num_list.length-2]) {
-                answer[answer.length-1] = num_list[num_list.length-1] - num_list[num_list.length-2];
-            } else {
-                answer[answer.length-1] = 2 * num_list[num_list.length-1];
+            if (i == num_list.length - 1) {
+                if (last > before) {
+                    answer[answer.length - 1] = last - before;
+                } else {
+                    answer[answer.length - 1] = last * 2;
+                }
             }
+            answer[i] = num_list[i];
+            
         }
+        
         return answer;
     }
 }
