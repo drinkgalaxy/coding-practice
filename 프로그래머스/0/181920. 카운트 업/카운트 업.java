9e4bd035ca76ff32eaa948b5570966dcc9ec1ebc
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-08-05 11:13:22 +0900

    [level 0] Title: 카운트 업, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -1,9 +1,11 @@
 class Solution {
     public int[] solution(int start_num, int end_num) {
         int[] answer = new int[end_num - start_num + 1];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = i+start_num;
+        int index = 0;
+        for (int i = start_num; i <= end_num; i++) {
+            answer[index++] = i;
         }
+        
         return answer;
     }
 }
