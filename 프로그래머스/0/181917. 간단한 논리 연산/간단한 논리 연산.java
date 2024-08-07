8302e3d793e15eb70f4064040eca54c5bf5332b0
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-08-07 11:17:52 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.03 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.java
@@ -1,26 +1,5 @@
 class Solution {
     public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
-
-        if (function(x1, x2)) {
-            if (function(x3, x4)) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
-    
-    public boolean function(boolean a, boolean b) {
-        boolean result = false;
-        if (a) {
-            result = true;
-        } else {
-            if (b) {
-                result = true;
-            }
-        }
-        return result;
+        return (x1||x2) && (x3||x4);
     }
 }
