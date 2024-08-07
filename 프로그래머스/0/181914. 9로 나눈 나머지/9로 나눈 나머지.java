962da9c430dbd24e257e7d65013bace4b8eb745d
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-08-07 14:21:47 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 60.29 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -3,16 +3,11 @@ import java.math.BigInteger;
 class Solution {
     public int solution(String number) {
         int digits = 0;
-        int result = 0;
+ 
         for (String s : number.split("")) {
             digits += Integer.valueOf(s);
         }
         
-        BigInteger bigInteger = new BigInteger(number);
-        
-        if (bigInteger.mod(BigInteger.valueOf(9)).intValue() == digits % 9) {
-            result = bigInteger.mod(BigInteger.valueOf(9)).intValue();
-        }
-        return result;
+        return digits % 9;
     }
 }
