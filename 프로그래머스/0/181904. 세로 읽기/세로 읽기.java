b0b149d83160c0c213ec33f33a90f4780906b1b9
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-08-12 14:37:12 +0900

    [level 0] Title: 세로 읽기, Time: 0.38 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -4,15 +4,12 @@ class Solution {
         String[] save = new String[my_string.length() / m];
 
         int index = 0;
-        int start = 0;
-        int end = m;
         for (int i = 0; i < my_string.length(); i = i + m) {
-            save[index] = my_string.substring(i, end + i);
+            save[index] = my_string.substring(i, m + i);
             index++;
         }
 
         for (String s : save) {
-            System.out.println(s);
             sb.append(s.charAt(c - 1));
         }
     
