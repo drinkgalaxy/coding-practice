2e9f848f6ca995320490294a6f63e5e02d72835c
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-08-16 15:05:09 +0900

    [level 0] Title: 이진수 더하기, Time: 0.04 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
@@ -1,38 +1,17 @@
 class Solution {
     public String solution(String bin1, String bin2) {
-        int result = ChangetoNumber(bin1) + ChangetoNumber(bin2);
-        return ChangetoBinary(result);
+        int result = changeToNumber(bin1) + changeToNumber(bin2);
+        return changeToBinary(result);
     }
     
-    public int ChangetoNumber(String str) {
-        int number = Integer.parseInt(str);
-        int digit = 0;
-        int powNumber = 0;
-        int count = 0;
-        
-        while (number != 0) {
-            digit = number % 10; // 각 자릿수 더하기
-            if (digit == 1) {
-                powNumber += Math.pow(2, count);   
-            }
-            number /= 10; // 다음 자릿수로 이동
-            count++;
-        }
-        
-        return powNumber;
+    public int changeToNumber(String b) {
+        return Integer.parseInt(b, 2);
     }
     
-    public String ChangetoBinary(int n) {
+    public String changeToBinary(int n) {
         if (n == 0) {
             return "0";
         }
-        
-        StringBuilder binary = new StringBuilder();
-        while (n > 0) {
-            binary.append(n % 2);
-            n /= 2;
-        }
-        
-        return binary.reverse().toString();
-    }   
+        return Integer.toString(n, 2);
+    }
 }
