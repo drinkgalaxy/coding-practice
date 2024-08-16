37f6f6f89f32b94ba2e64dca35205c87458dd4a4
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-08-16 14:23:56 +0900

    [Silver III] Title: 칸토어 집합, Time: 168 ms, Memory: 24120 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -1,24 +1,24 @@
 import java.io.*;
 import java.util.Arrays;
 
+
 public class Main {
     static char[] lineArr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb;
 
-        String input; // 입력 값
+        String input;
 
-        while ((input = br.readLine()) != null) { // 파일의 끝에서 입력을 멈춘다. = 입력값이 없을 때까지 반복
+        while ((input = br.readLine()) != null) {
             int N = Integer.parseInt(input);
             sb = new StringBuilder();
 
-            int len = (int) Math.pow(3, N); // 3의 N승
-
-            lineArr = new char[len]; // 3의 N승 크기를 가진 배열
+            int line = (int) Math.pow(3, N);
+            lineArr = new char[line];
             Arrays.fill(lineArr, '-');
 
-            divide(0, len);
+            divide(0, line);
 
             for (int i = 0; i < lineArr.length; i++) {
                 sb.append(lineArr[i]);
@@ -28,23 +28,23 @@ public class Main {
         br.close();
     }
 
-    static void divide (int start, int length) {
-        if (length <= 1) {
-            return; // 재귀함수 종료조건 잊지말기
+    public static void divide(int start, int line) {
+        if (line <= 1) {
+            return;
+        }
+        // 0, 27
+        int length = line / 3; // 9
+
+        // 가운데 공백으로 바꾸기 9, 18
+        for (int i = start + length; i < start + length*2; i++) {
+            lineArr[i] = ' ';
         }
 
-        int newLength = length / 3;
+        // 첫 번째 구역 나누기. 시작 : 0, 길이 : 9
+        divide(start, length);
 
-        // 가운데 공백 (두 번째 구역) 으로 바꾸기
-        for (int a = start + newLength; a < start + newLength*2; a++) {
-            lineArr[a] = ' ';
-        }
+        // 세 번째 구역 나누기. 시작 : 18, 길이 : 9
+        divide(start + length * 2, length);
 
-        // 첫 번째 구역 쪼개기
-        divide(start, newLength);
-
-        // 세 번째 구역 쪼개기
-        divide(start + newLength*2, newLength);
     }
 }
-     
