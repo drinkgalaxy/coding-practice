23a7495d52d08d742f971015edea7b112d94933d
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-08-18 14:45:26 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.07 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -2,38 +2,14 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] str_list) {
-        ArrayList<String> list = new ArrayList<>();
         
-        String check = "";
-        for (String s : str_list) {
-            check += s;
-        }
-        
-        if (check.contains("l") || check.contains("r")) {
-            for (int i = 0; i < str_list.length; i++) {
-                if (str_list[i].equals("l")) {
-                    for (int j = 0; j < i; j++) {
-                        list.add(str_list[j]);
-                    }
-                    break;
-                } else if (str_list[i].equals("r")) {
-                    for (int j = i+1; j < str_list.length; j++) {
-                        list.add(str_list[j]);
-                    }
-                    break;
-                }
+        for (int i = 0; i < str_list.length; i++) {
+            if (str_list[i].equals("l")) {
+                return Arrays.copyOfRange(str_list, 0, i);
+            } else if (str_list[i].equals("r")) {
+                return Arrays.copyOfRange(str_list, i+1, str_list.length);
             }
-        } else {
-            String[] answer = new String[0];
-            return answer;
         }
-
-        String[] answer = new String[list.size()];
-        int index = 0;
-        for (String s : list) {
-            answer[index++] = s;
-        }
-        
-        return answer;
+        return new String[0];
     }
 }
