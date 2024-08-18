bfb5e3d0ff01a29fa1d80b8a05fd51186b8e68d8
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-08-18 14:57:51 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -1,11 +1,7 @@
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int len = 0;
-        if (num_list.length % n == 0) {
-            len = num_list.length / n;
-        } else {
-            len = num_list.length / n + 1;
-        }
+
+        int len = num_list.length % n == 0 ? num_list.length / n : num_list.length / n + 1;
         
         int[] answer = new int[len];
         int index = 0;
