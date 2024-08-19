f5ee18df69eadb2083b713e45170475fd09e9cd5
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-08-19 12:27:11 +0900

    [level 0] Title: 5명씩, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -1,27 +1,10 @@
-import java.util.*;
-
 class Solution {
     public String[] solution(String[] names) {
-        Deque<String> deque = new ArrayDeque<>();
-        ArrayList<String> list = new ArrayList<>();
-        
-        for (int i = 0; i < names.length; i++) {
-            if (deque.size() == 5) {
-                list.add(deque.poll());
-                deque = new ArrayDeque<>();
-            }
-            deque.offer(names[i]);
-        }
-        
-        // 남은 이름까지 넣기
-        list.add(deque.poll());
-        
-        String[] answer = new String[list.size()];
+        String[] answer = new String[names.length % 5 == 0 ? names.length / 5 : names.length / 5 + 1];
         int index = 0;
-        for (String s : list) {
-            answer[index++] = s;
+        for (int i = 0; i < names.length; i += 5) {
+            answer[index++] = names[i];
         }
-        
         return answer;
     }
 }
