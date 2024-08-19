d6b669306ffa225fd4995cd9d5119c45554f6427
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-08-19 12:39:03 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] numbers, int n) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (sum > n) {
+                return sum;
+            }
+            sum += numbers[i];
+        }
+        
+        return sum;
+    }
+}
