c8ce687358edee182917e837776ae4f9344b5af3
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-08-22 10:55:14 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.29 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -2,17 +2,6 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String my_string) {
-        ArrayList<String> list = new ArrayList<>();
-        
-        for (String s : my_string.split(" ")) {
-            list.add(s);
-        }
-        
-        String[] answer = new String[list.size()];
-        int index = 0;
-        for (String s : list) {
-            answer[index++] = s;
-        }
-        return answer;
+        return my_string.split(" ");
     }
 }
