5c3c488c1018c49f3b267c5f3858a0bfcaaf2860
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-08-27 11:15:38 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.51 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -3,10 +3,9 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] num_list) {
         Arrays.sort(num_list);
-        int index = 0; 
         int[] answer = new int[num_list.length - 5];
         for (int i = 5; i < num_list.length; i++) {
-            answer[index++] = num_list[i];
+            answer[i - 5] = num_list[i];
         }
         return answer;
     }
