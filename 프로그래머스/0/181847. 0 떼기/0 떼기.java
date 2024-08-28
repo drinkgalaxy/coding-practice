221b1e0b5415d1b91e1565cb09ba74a59dba3047
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-08-28 11:07:15 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,20 +1,6 @@
 class Solution {
     public String solution(String n_str) {
-        StringBuilder sb = new StringBuilder();
         
-        int count = 0;
-        for (String s : n_str.split("")) {
-            if (s.equals("0")) {
-                count++;
-            } else {
-                break;
-            }
-        }
-        
-        for (int i = count; i < n_str.length(); i++) {
-            sb.append(n_str.charAt(i));
-        }
-        
-        return sb.toString();
+        return String.valueOf(Integer.parseInt(n_str));
     }
 }
