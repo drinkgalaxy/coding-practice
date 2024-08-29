d789fdbb6d93d393044d68e482b8ca06b14a2994
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-08-29 16:28:21 +0900

    [Silver IV] Title: 동전 0, Time: 68 ms, Memory: 11536 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,27 +1,27 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken()); // 동전 종류의 개수
+        int K = Integer.parseInt(st.nextToken()); // 목적 가치의 합
+
         int[] money = new int[N];
 
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            money[i] = Integer.parseInt(st.nextToken());
+            money[i] = Integer.parseInt(br.readLine());
         }
-        int count = 0;
 
-        // 가치가 큰 동전부터 선택해야 개수를 최소로 구성할 수 있음
-        for (int i = N-1; i >= 0; i--) {
+        int count = 0;
+        for (int i = money.length-1; i >= 0; i--) {
             if (money[i] <= K) {
                 count += K / money[i];
                 K = K % money[i];
             }
         }
+
         System.out.println(count);
     }
 }
