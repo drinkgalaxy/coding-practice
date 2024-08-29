03feee89c775de85819159f8ef1971383ecbf099
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-08-29 11:02:42 +0900

    [level 0] Title: 정수 찾기, Time: 0.04 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,13 +1,10 @@
 class Solution {
     public int solution(int[] num_list, int n) {
-        int count = 0;
-        for (int i = 0; i < num_list.length; i++) {
-            if (num_list[i] == n) {
-                count = 1;
-            } 
+        for (int i : num_list) {
+            if (i == n) {
+                return 1;
+            }
         }
-        return count;
+        return 0;
     }
-}    
-    
-    
+}
