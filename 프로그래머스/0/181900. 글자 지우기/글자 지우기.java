75e9908a01e95968dea04168adb9491ea730ffdf
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-09-04 10:41:45 +0900

    [level 0] Title: 글자 지우기, Time: 8.48 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -1,14 +1,16 @@
 class Solution {
     public String solution(String my_string, int[] indices) {
-        StringBuilder sb = new StringBuilder();
-        
         String[] my_arr = my_string.split("");
-        for (int i = 0; i < indices.length; i++) {
-            my_arr[indices[i]] = "";
+        
+        for (int i : indices) {
+            my_arr[i] = "";
         }
         
+        StringBuilder sb = new StringBuilder();
         for (String s : my_arr) {
-            sb.append(s);
+            if (!s.equals("")) {
+                sb.append(s);
+            }
         }
         
         return sb.toString();
