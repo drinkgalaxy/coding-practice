c2a04926af62997d5c0bc7e3464bbf4eaada4fbb
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-09-04 13:52:22 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.05 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        
+        answer[0] = GCD(n, m);
+        answer[1] = LCM(n, m);
+        
+        return answer;
+    }
+    
+    public int GCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return GCD(b, a % b);
+        }
+    }
+    
+    public int LCM(int a, int b) {
+        return (a * b) / GCD(a, b);
+    }
+}
