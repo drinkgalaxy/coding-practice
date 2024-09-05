82f6b5927e5b24777683a5094ec3cf0dbf381a8a
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-09-05 14:14:34 +0900

    [Silver IV] Title: 수 찾기, Time: 1476 ms, Memory: 70640 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,43 +1,51 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(A);
 
-        int M = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine()); // 자연수 N 개수
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int M = Integer.parseInt(br.readLine()); // 자연수 M 개수
+
+        st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < M; i++) {
             int target = Integer.parseInt(st.nextToken());
-            boolean find = false;
-
-            // 이진 탐색 수행
-            int start = 0;
-            int end = A.length-1;
-            while (start <= end) {
-                int midi = (start + end) / 2;
-                int midv = A[midi];
-                if (midv > target) {
-                    end = midi-1;
-                } else if (midv < target) {
-                    start = midi+1;
-                } else {
-                    find = true;
-                    break;
-                }
-            }
-            if (find) {
+            if (binarySearch(arr, target)) {
                 System.out.println(1);
             } else {
                 System.out.println(0);
             }
         }
+    }
+
+    public static boolean binarySearch(int[] arr, int target) {
+        boolean find = false;
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int midIndex = (start + end) / 2;
+
+            if (target < arr[midIndex]) {
+                end = midIndex - 1;
+            } else if (target > arr[midIndex]) {
+                start = midIndex + 1;
+            } else {
+                find = true;
+                break;
+            }
+        }
+        return find;
     }
 }
