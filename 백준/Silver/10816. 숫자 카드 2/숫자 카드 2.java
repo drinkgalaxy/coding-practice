54d5b08774c38c66f8c483acc46385eb8b748ee1
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-09-08 15:22:00 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1088 ms, Memory: 180340 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,27 +1,67 @@
-
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int[] cards;
+    static int[] answer;
+    static int N, M;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Map<Long, Long> map = new HashMap<>();
-        long N = Long.valueOf(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(br.readLine()); // 상근이가 가진 숫자 카드 개수
+        cards = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
-            long key = Long.valueOf(st.nextToken());
-            map.put(key, map.getOrDefault(key, Long.valueOf(0)) + 1);
+            cards[i] = Integer.parseInt(st.nextToken());
         }
 
-        long M = Long.valueOf(br.readLine());
-        st = new StringTokenizer(br.readLine());
+        // 상근이 배열 정렬
+        Arrays.sort(cards);
+
+        M = Integer.parseInt(br.readLine()); // 비교 카드들 M개
+
         StringBuilder sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < M; i++) {
-            long key = Long.valueOf(st.nextToken());
-            sb.append(map.getOrDefault(key, Long.valueOf(0))).append(" ");
+            int target = Integer.parseInt(st.nextToken());
+            sb.append(upperBound(target) - lowerBound(target)).append(" ");
         }
 
         System.out.println(sb);
+    }
+    public static int lowerBound(int target) {
+        int lo = 0;
+        int hi = cards.length;
+
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+
+            // 중복 원소일 경우 왼쪽으로 탐색해야하므로 내림.
+            if (target <= cards[mid]) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+
+    public static int upperBound(int target) {
+        int lo = 0;
+        int hi = cards.length;
+
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+
+            // 중복 원소 보다 큰 값이 상한값임.
+            if (target < cards[mid]) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return hi;
     }
 }
