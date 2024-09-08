707e596e94ccbf6e687f0fd6f91c65e97859ced5
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-09-08 14:15:49 +0900

    [Silver I] Title: 정수 삼각형, Time: 196 ms, Memory: 26680 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -4,44 +4,39 @@ import java.util.StringTokenizer;
 public class Main {
     static int[][] arr;
     static Integer[][] dp;
-    static int N;
+    static int n;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine()); // 삼각형의 크기
 
-        arr = new int[N][N];
-        dp = new Integer[N][N];
-        StringTokenizer st;
-        // 데이터 저장
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(br.readLine()); // 삼각형의 크기
 
+        arr = new int[n][n];
+        dp = new Integer[n][n];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             for (int j = 0; j < i + 1; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        // dp 삼각형 맨 밑의 값으로 초기화
-        for (int i = 0; i < N; i++) {
-            dp[N - 1][i] = arr[N - 1][i];
+        // 초기화 -> 삼각형 맨 밑의 값으로 초기화
+        for (int i = 0; i < n; i++) {
+            dp[n - 1][i] = arr[n - 1][i];
         }
-
         System.out.println(getMax(0, 0));
     }
 
-    public static int getMax(int depth, int index) {
-        // 마지막 행일 경우 현재 위치의 dp값 반환
-        if (depth == N - 1) {
-            return dp[depth][index];
+    private static int getMax(int depth, int index) {
+        // 초기화된 맨 밑의 값이면
+        if (depth == n - 1) {
+            return dp[n - 1][index];
         }
 
-        // 탐색하지 않은 값에 메모이제이션 저장
         if (dp[depth][index] == null) {
-            dp[depth][index] = Math.max(getMax(depth + 1, index), getMax(depth + 1, index + 1)) + arr[depth][index];
+            dp[depth][index] = Math.max(getMax(depth + 1, index) + arr[depth][index], getMax(depth + 1, index + 1) + arr[depth][index]);
         }
 
-        // 리턴
         return dp[depth][index];
     }
-
 }
