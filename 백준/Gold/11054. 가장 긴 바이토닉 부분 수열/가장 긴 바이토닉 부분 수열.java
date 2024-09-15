86972bf860d06aafdc864975de4c7993ad995f1b
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-09-16 00:23:48 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 124 ms, Memory: 19740 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -1,67 +1,60 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+
 public class Main {
+    static int[] arr;
     static Integer[] LIS_dp;
     static Integer[] LDS_dp;
-    static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(br.readLine()); // 수열의 크기
+        arr = new int[N];
+        LIS_dp = new Integer[N];
+        LDS_dp = new Integer[N];
 
-        LIS_dp = new Integer[n];
-        LDS_dp = new Integer[n];
-        arr = new int[n];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 바이토닉 수열은 점점 커지거나, 점점 작아지거나, 커졌다작아지거나
-
-        for (int i = 0; i < n; i++) {
+        // LIS, LDS 각각 수행
+        for (int i = 0; i < N; i++) {
             LIS(i);
             LDS(i);
         }
 
         int max = -1;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             max = Math.max(LIS_dp[i] + LDS_dp[i], max);
         }
 
-        System.out.println(max - 1);
-
+        System.out.println(max-1);
     }
 
-    // LIS (최장 증가 부분수열)
-    private static int LIS(int n) {
-
+    public static int LIS(int n) {
         if (LIS_dp[n] == null) {
             LIS_dp[n] = 1;
 
-            // n 이전의 노드들을 탐색
-            for (int i = n - 1; i >= 0; i--) {
-                // 이전의 노드 중 arr[n] 의 값보다 작은 걸 발견했을 경우
+            for (int i = n-1; i >= 0; i--) {
                 if (arr[i] < arr[n]) {
-                    LIS_dp[n] = Math.max(LIS_dp[n], LIS(i) + 1);
+                    LIS_dp[n] = Math.max(LIS_dp[n], LIS(i)+1);
                 }
             }
         }
         return LIS_dp[n];
     }
 
-    // LDS (최장 감소 부분수열)
-    private static int LDS(int n) {
-
+    public static int LDS(int n) {
         if (LDS_dp[n] == null) {
             LDS_dp[n] = 1;
-            // n 이후의 노드들을 탐색
-            for (int i = n + 1; i < LDS_dp.length; i++) {
-                // 이후의 노드 중 arr[n] 의 값보다 작은 걸 발견했을 경우
+
+            for (int i = n+1; i < LDS_dp.length; i++) {
+                // 역방향으로 탐색
                 if (arr[i] < arr[n]) {
-                    LDS_dp[n] = Math.max(LDS_dp[n], LDS(i) + 1);
+                    LDS_dp[n] = Math.max(LDS_dp[n], LDS(i)+1);
                 }
             }
         }
