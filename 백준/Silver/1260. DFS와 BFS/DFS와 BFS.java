39cbd3044feaa68f401a8ba51a5460c087ba7438
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-09-16 00:51:19 +0900

    [Silver II] Title: DFS와 BFS, Time: 148 ms, Memory: 17224 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,67 +1,76 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
-    public static ArrayList<Integer>[] A;
-    public static boolean visited[];
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int V = Integer.parseInt(st.nextToken());
 
-        // A 인접 리스트 초기화
-        A = new ArrayList[N+1];
-        for (int i = 1; i <= N; i++) {
-            A[i] = new ArrayList<>();
+        visited = new boolean[N+1];
+
+        // 초기화 & 저장
+        for (int i = 0; i < N+1; i++) {
+            graph.add(new ArrayList<>());
         }
 
-        // A 인접 리스트에 그래프 데이터 저장하기
         for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            A[start].add(end);
-            A[end].add(start);
+            st = new StringTokenizer(br.readLine(), " ");
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph.get(u).add(v);
+            graph.get(v).add(u);
         }
 
-        // 정점 번호가 작은 것을 먼저 방문하기 위해 A 정렬
+        // 오름차순 정렬
         for (int i = 1; i <= N; i++) {
-            Collections.sort(A[i]);
+            Collections.sort(graph.get(i));
         }
 
-        // 방문 배열 리스트 초기화 후 DFS, BFS 실행
+        dfs(V);
+        System.out.println(sb);
         visited = new boolean[N+1];
-        DFS(V);
-        System.out.println();
-        visited = new boolean[N+1];
-        BFS(V);
-        System.out.println();
+        sb = new StringBuilder();
+        bfs(V);
+        System.out.println(sb);
     }
 
-    public static void DFS(int node) {
-        System.out.print(node+" ");
+    public static void dfs(int node) {
         visited[node] = true;
-        for (int i : A[node]) {
-            if (!visited[i]) {
-                DFS(i);
+        sb.append(node).append(" ");
+
+        for (int i = 0; i < graph.get(node).size(); i++) {
+            int nextNode = graph.get(node).get(i);
+            if (!visited[nextNode]) {
+                dfs(nextNode);
             }
         }
     }
 
-    public static void BFS(int node) {
-    Queue<Integer> queue = new LinkedList<>();
-    queue.add(node);
-    visited[node] = true;
+    public static void bfs(int node) {
+        Queue<Integer> queue = new LinkedList<>();
 
-    while (!queue.isEmpty()) {
-        int nowNode = queue.poll();
-        System.out.print(nowNode+" ");
-        for (int i : A[nowNode]) {
-            if (!visited[i]) {
-                visited[i] = true;
-                queue.add(i);
+        visited[node] = true;
+        queue.offer(node);
+        sb.append(node).append(" ");
+
+        while (!queue.isEmpty()) {
+            int n = queue.poll();
+
+            for (int i = 0; i < graph.get(n).size(); i++) {
+                int nextNode = graph.get(n).get(i);
+                if (!visited[nextNode]) {
+                    visited[nextNode] = true;
+                    queue.offer(nextNode);
+                    sb.append(nextNode).append(" ");
                 }
             }
         }
