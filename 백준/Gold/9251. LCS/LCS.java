9b77a65ab109a140005abb0f75c38f104058d2ab
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-09-17 15:29:32 +0900

    [Gold V] Title: LCS, Time: 144 ms, Memory: 25876 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,10 +1,11 @@
 import java.io.*;
 
+
+
 public class Main {
     static char[] str1;
     static char[] str2;
     static Integer[][] dp;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -14,25 +15,23 @@ public class Main {
         dp = new Integer[str1.length][str2.length];
 
         System.out.println(LCS(str1.length - 1, str2.length - 1));
-
     }
 
-    public static int LCS(int x, int y) {
-        // 인덱스 밖 (공집합) 일 경우 0 반환
+    private static int LCS(int x, int y) {
+        // 인덱스 밖 값일 경우 0 반환
         if (x == -1 || y == -1) {
             return 0;
         }
 
-        // 만약 탐색하지 않은 인덱스일 때
         if (dp[x][y] == null) {
-            dp[x][y] = 0;
+            dp[x][y] = 0; // 초기화
 
-            // str1의 x번째 문자와 str2의 y번째 문자가 같은지 검사
+            // str1 의 x번째 문자와 str2 의 y 번째 문자가 같은지 검사
             if (str1[x] == str2[y]) {
-                dp[x][y] = LCS(x - 1, y - 1) + 1;
+                dp[x][y] = LCS(x-1, y-1) + 1;
             } else {
-                // 같지 않다면 LCS(dp[x-1][y]) 와 LCS(dp[x][y-1]) 중 큰 값으로 초기화
-                dp[x][y] = Math.max(LCS(x-1, y), LCS(x, y - 1));
+                // 같지 않다면 더 큰 값 저장 
+                dp[x][y] = Math.max(LCS(x, y-1), LCS(x-1, y));
             }
         }
         return dp[x][y];
