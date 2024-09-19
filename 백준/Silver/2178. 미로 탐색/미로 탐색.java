7c818c841c4f8c84a28f41006086ee75fad2398e
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-09-19 15:46:34 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 12352 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,53 +1,64 @@
 import java.io.*;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 
 public class Main {
-    // 2차원 배열을 사용해야함
-    // 상하좌우를 탐색하기 위한 배열 선언하기
-    public static int[] dx = {0, 1, 0, -1};
-    public static int[] dy = {1, 0, -1, 0};
-    public static boolean[][] visited;
-    public static int[][] A;
-    public static int N;
-    public static int M;
-
-
+    static char[][] miro;
+    static int[][] dist; // 방문 배열 대신 최단 거리를 계산할 dist 배열
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+    static int N, M;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        A = new int[N][M];
-        visited = new boolean[N][M];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken()); // 미로의 행 N
+        M = Integer.parseInt(st.nextToken()); // 미로의 열 M
+        miro = new char[N][M];
+        dist = new int[N][M];
+
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            String line = st.nextToken();
+            String line = br.readLine();
             for (int j = 0; j < M; j++) {
-                A[i][j] = Integer.parseInt(line.substring(j, j+1));
+                miro[i][j] = line.charAt(j);
+                dist[i][j] = -1; // 거리를 -1로 세팅해 방문여부 확인
             }
         }
-        BFS(0, 0);
-        System.out.println(A[N-1][M-1]);
+        dist[0][0] = 1; // 첫 시작 (출발 지점 포함)
+        bfs(0, 0);
+
+        System.out.println(dist[N-1][M-1]); 
     }
 
-    public static void BFS(int i, int j) {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {i, j}); // 큐에서 노드 데이터 가져오기
-        visited[i][j] = true;
+    public static void bfs(int x, int y) {
+        Queue<Node> queue = new LinkedList<>();
 
-        while(!queue.isEmpty()) {
-            int now[] = queue.poll();
-            for (int k = 0; k < 4; k++) { // 상하좌우 탐색
-                int x = now[0] + dx[k];
-                int y = now[1] + dy[k];
-                if (x >= 0 && y >= 0 && x < N && y < M) { // 좌표 유효성 검사
-                    if (A[x][y] != 0 && !visited[x][y]) {
-                        visited[x][y] = true;
-                        A[x][y] = A[now[0]][now[1]] + 1; // 깊이 업데이트
-                        queue.add(new int[] {x, y});
+        queue.offer(new Node(x, y));
+
+        while (!queue.isEmpty()) {
+            Node n = queue.poll();
+            // 상하좌우 탐색
+            for (int i = 0; i < 4; i++) {
+                int nx = dx[i] + n.x;
+                int ny = dy[i] + n.y;
+                if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+                    if (miro[nx][ny] == '1' && dist[nx][ny] == -1) {
+                        dist[nx][ny] = dist[n.x][n.y] + 1; // 한칸 이동했기 때문에, 이전 출발지점의 거리 + 1을 해줌
+                        queue.offer(new Node(nx, ny));
                     }
                 }
             }
+        }
+    }
+
+    public static class Node {
+        int x, y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 }
