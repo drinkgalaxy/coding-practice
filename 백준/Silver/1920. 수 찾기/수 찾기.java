a45aa28cb51804669a8b6e59afa50f64e1f6b097
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-09-26 14:28:28 +0900

    [Silver IV] Title: 수 찾기, Time: 524 ms, Memory: 57196 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,12 +2,13 @@ import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine()); // 자연수 N 개수
+        int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -17,35 +18,35 @@ public class Main {
 
         Arrays.sort(arr);
 
-        int M = Integer.parseInt(br.readLine()); // 자연수 M 개수
+        int M = Integer.parseInt(br.readLine());
 
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < M; i++) {
             int target = Integer.parseInt(st.nextToken());
-            if (binarySearch(arr, target)) {
-                System.out.println(1);
+
+            int l = 0;
+            int r = arr.length-1;
+            boolean find = false;
+
+            while (l <= r) {
+                int midIndex = (l + r) / 2;
+
+                if (arr[midIndex] < target) {
+                    l = midIndex+1;
+                } else if (target < arr[midIndex]) {
+                    r = midIndex-1;
+                } else {
+                    find = true;
+                    break;
+                }
+            }
+
+            if (find) {
+                sb.append(1).append("\n");
             } else {
-                System.out.println(0);
+                sb.append(0).append("\n");
             }
         }
-    }
-
-    public static boolean binarySearch(int[] arr, int target) {
-        boolean find = false;
-        int start = 0;
-        int end = arr.length - 1;
-        while (start <= end) {
-            int midIndex = (start + end) / 2;
-
-            if (target < arr[midIndex]) {
-                end = midIndex - 1;
-            } else if (target > arr[midIndex]) {
-                start = midIndex + 1;
-            } else {
-                find = true;
-                break;
-            }
-        }
-        return find;
+        System.out.println(sb);
     }
 }
