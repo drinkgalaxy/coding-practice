5823d96034ad842d3ddd0eddd16342b05ce6cac3
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-09-27 11:36:35 +0900

    [level 3] Title: 금과 은 운반하기, Time: 145.37 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.java
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.java
@@ -1,64 +1,53 @@
 class Solution {
     public long solution(int a, int b, int[] g, int[] s, int[] w, int[] t) {
-        long answer = -1;
-        
         long min = 0L;
+        // 최대 적재량이 1이고 편도 이동시간이 100000 일 때 시간 최대
+        // => (a+b)의 최댓값 * 왕복 최대 시간
         long max = 400000000000000L;
         long mid;
         
-        // 이분 탐색
         while (min < max) {
             mid = (min + max) / 2;
             
-            // mid 시간에 (a, b) kg 를 옮길 수 있는가?
-            if (IsPossible(mid, a, b, g, s, w, t)) {
+            if (IsPossible(mid, a, b, g, s, w, t)) { // 만족하면 더 작은 최소시간 시도
                 max = mid;
             } else {
-                min = mid + 1;
+                min = mid+1;
             }
         }
         return max;
     }
     
-    public boolean IsPossible(long time, int a, int b, int[] g, int[] s, int[] w, int[] t) {
-        int n = g.length; 
-        // time 시간 동안 옮길 수 있는 최대 금, 은 적재 가능 무게
-        long totalG = 0L;
-        long totalS = 0L;
-        long total = 0L;
+    boolean IsPossible(long time, int a, int b, int[] g, int[] s, int[] w, int[] t) {
+        long maxTotalG = 0L;
+        long maxTotalS = 0L;
+        long maxTotalGS = 0L;
+        long maxG, maxS, maxGS = 0L;
         
-        // i 번째 도시에서 time 시간동안 옮길 수 있는 최대 금, 은 적재 가능 무게
-        long maxG, maxS, maxT;
-        
-        // time 시간 동안 편도로 움직일 수 있는 횟수
-        long count;
-        
-        for (int i = 0; i < n; i++) {
-            count = time / (t[i] * 2L); // time 시간 / 왕복 시간 = 편도로 움직일 수 있는 횟수
+        long count = 0L;
+        for (int i = 0; i < g.length; i++) {
+            count = time / (t[i] * 2L); // time / 왕복 시간 -> 움직일 수 있는 횟수
             if (time % (t[i] * 2L) >= t[i]) {
-                count++;
+                count++; // 혹시 남은 편도까지 계산
             }
-            
-            // count 번 움직여서 옮길 수 있는 최대랑 vs (가진 금 총량, 가진 은 총량, 가진 금+은 총량)
+            // count 번 움직여서 옮길 수 있는 최대량 vs 가진 금속 총량
             maxG = Math.min(w[i] * count, g[i]);
             maxS = Math.min(w[i] * count, s[i]);
-            maxT = Math.min(w[i] * count, g[i] + s[i]);
+            maxGS = Math.min(w[i] * count, g[i] + s[i]);
             
-            totalG += maxG;
-            totalS += maxS;
-            total += maxT;
+            maxTotalG += maxG;
+            maxTotalS += maxS;
+            maxTotalGS += maxGS;
         }
         
-        // count 번 금/은 하나만 옮겼는데 필요량 충족 X => 시간 내에 불가능
-        if (totalG < a || totalS < b) {
+        // count 번 금 또는 은 옮겼는데 필요량 충족 X -> 시간 내에 못 옮김
+        if (maxTotalG < a || maxTotalS < b) {
             return false;
         }
-        
-        // count 번 옮길 수 있는 최대를 옮겼는데 필요량 충족 X => 시간 내에 불가능
-        if (total < a + b) {
+        // count 번 금과은 옮겼는데 필요량 충족 X -> 시간 내에 못 옮김
+        if (maxTotalGS < a+b) {
             return false;
         }
-        
         return true;
     }
 }
