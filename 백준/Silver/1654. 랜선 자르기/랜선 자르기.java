b864ae68bc9914acf4794762327901d725e54c3e
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-09-27 10:56:48 +0900

    [Silver II] Title: 랜선 자르기, Time: 120 ms, Memory: 14800 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -7,14 +7,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int K = Integer.parseInt(st.nextToken()); // 가진 랜선의 개수
-        int N = Integer.parseInt(st.nextToken()); // 필요한 랜선의 개수
+        /*
+        K 개의 랜선을 잘라 N개의 같은 길이의 랜선으로 만들어야 함.
+        이때 만들 수 있는 최대 랜선의 길이
+        -> 이분탐색으로 상한값-1 출력
+         */
+        int K = Integer.parseInt(st.nextToken()); // 원래 가진 랜선의 개수
+        long N = Integer.parseInt(st.nextToken()); // 필요한 랜선의 개수
 
         int[] arr = new int[K];
 
-        long max = 0;
-
-        // 입력과 동시에 해당 랜선의 길이가 최댓값인지를 확인하며 max 갱신
+        long max = 0; // 최댓값 갱신
         for (int i = 0; i < K; i++) {
             arr[i] = Integer.parseInt(br.readLine());
             if (max < arr[i]) {
@@ -22,34 +25,24 @@ public class Main {
             }
         }
 
-        // max 에서 +1 값이어야 한다.
-        // mid 가 0이 되어서 0을 나눗셈하는 걸 막기 위함이다.
-        max++;
-
+        max++; // 나눴을 때 0이 됨을 막기위해 max+1을 해줌.
         long min = 0;
         long mid = 0;
 
         while (min < max) {
-            mid = (min + max) / 2; // 범위 내에서 중간 길이
+            mid = (min + max) / 2;
 
             long count = 0;
-
-            // 구한 중간 길이로 잘라서 총 몇 개가 만들어지는지 구함.
             for (int i = 0; i < arr.length; i++) {
                 count += (arr[i] / mid);
             }
 
-            // mid 길이로 잘랐을 때의 개수가 만들고자 하는 랜선의 개수보다 작다면
-            // 최대 길이를 줄인다.
-            // 그 외에는 최소 길이를 늘린다.
             if (count < N) {
                 max = mid;
             } else {
-                min = mid + 1;
+                min = mid+1;
             }
         }
-
-        System.out.println(min - 1);
-        
+        System.out.println(min-1);
     }
 }
