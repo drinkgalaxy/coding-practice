35f756b2d32ff2918198c8f1fceaed9f74eeefa2
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-10-03 14:53:12 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.94 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -3,14 +3,10 @@ class Solution {
     public int solution(int []A, int []B) {
         Arrays.sort(A);
         Arrays.sort(B);
-        int[] newB = new int[B.length];
-        for (int i = 0; i < B.length; i++) {
-            newB[i] = B[B.length-1-i];
-        }
-        
+
         int sum = 0;
         for (int i = 0; i < A.length; i++) {
-            sum += (A[i] * newB[i]);
+            sum += (A[i] * B[B.length-1-i]);
         }
 
         return sum;
