93514e4753f8e6ea6c9727686e9a4385848dba00
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-10-03 11:39:49 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 424 ms, Memory: 125348 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -1,13 +1,13 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
+
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine()); // 수열 A 의 크기
-
+        int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
         int[] LIS = new int[N];
 
@@ -16,34 +16,30 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // LIS 초기 값으로 첫 번째 수열의 값을 갖는다.
         LIS[0] = arr[0];
         int lengthOfLIS = 1;
 
         for (int i = 1; i < N; i++) {
             int key = arr[i];
 
-            // 만약 key 가 LIS 의 마지막 값보다 클 경우 추가
-            if (LIS[lengthOfLIS - 1] < key) {
+            if (key > LIS[lengthOfLIS-1]) {
                 lengthOfLIS++;
-                LIS[lengthOfLIS - 1] = key;
-            // 아닐 경우 탐색하는 값보다 큰 가장 가까운 원소를 찾기 위해
-            // Lower Bound 이분탐색 진행
+                LIS[lengthOfLIS-1] = key;
             } else {
-                int lo = 0;
-                int hi = lengthOfLIS;
+                // 탐색하는 값보다 큰, 가장 가까운 원소를 찾아야됨 -> 하한값 탐색
+                int min = 0;
+                int max = lengthOfLIS;
 
-                while (lo < hi) {
-                    int mid = (lo + hi) / 2;
+                while (min < max) {
+                    int mid = (min + max) / 2;
 
                     if (key <= LIS[mid]) {
-                        hi = mid;
+                        max = mid;
                     } else {
-                        lo = mid + 1;
+                        min = mid+1;
                     }
                 }
-                // lo 가 LIS 에서 대치 될 원소의 위치가 됨.
-                LIS[lo] = key;
+                LIS[max] = key;
             }
         }
 
