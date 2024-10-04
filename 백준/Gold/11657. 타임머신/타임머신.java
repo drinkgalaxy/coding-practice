6a61a70ef37ddfd0e1966aa55568f657cf39a33a
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-10-04 17:33:10 +0900

    [Gold IV] Title: 타임머신, Time: 224 ms, Memory: 17196 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.java
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -1,58 +1,53 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-// 에지 리스트를 다루기 위해 클래스 별도 구현
-class Edge {
-    int start, end, time;
-
-    public Edge(int start, int end, int time) {
-        this.start = start;
-        this.end = end;
-        this.time = time;
-    }
-}
 
 public class Main {
-    static int N, M;
-    static Edge edges[];
-    static long distance[];
-
+    static Edge[] edge;
+    static long[] distance;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        edges = new Edge[M+1];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken()); // 도시의 개수
+        int M = Integer.parseInt(st.nextToken()); // 버스 노선의 개수
+
         distance = new long[N+1];
-        Arrays.fill(distance, Integer.MAX_VALUE); // 거리배열 초기화
-        for (int i = 0; i < M; i++) { // 에지 리스트에 데이터 저장
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int time = Integer.parseInt(st.nextToken());
-            edges[i] = new Edge(start, end, time);
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        edge = new Edge[M+1];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+
+            edge[i] = new Edge(A, B, C);
         }
 
         // 벨만-포드 알고리즘 수행
-        distance[1] = 0;
+        distance[1] = 0; // 출발지점
         for (int i = 1; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                Edge edge = edges[j];
-                if (distance[edge.start] != Integer.MAX_VALUE
-                        && distance[edge.end] > distance[edge.start] + edge.time) {
-                    distance[edge.end] = distance[edge.start] + edge.time;
+                Edge e = edge[j];
+                // 더 작은 최단거리가 있으면 업데이트
+                if (distance[e.start] != Integer.MAX_VALUE &&
+                    distance[e.end] > distance[e.start] + e.weight) {
+                    distance[e.end] = distance[e.start] + e.weight;
                 }
             }
         }
-        // 음수 사이클의 유무 확인
+        // 음수 사이클 확인하기
         boolean mCycle = false;
         for (int i = 0; i < M; i++) {
-            Edge edge = edges[i];
-            if (distance[edge.start] != Integer.MAX_VALUE
-                    && distance[edge.end] > distance[edge.start] + edge.time) {
-                mCycle = true; // 값 업데이트 대신 mCycle을 true로 변경
+            Edge e = edge[i];
+            if (distance[e.start] != Integer.MAX_VALUE &&
+                    distance[e.end] > distance[e.start] + e.weight) {
+                mCycle = true;
             }
         }
+
         if (!mCycle) {
             for (int i = 2; i <= N; i++) {
                 if (distance[i] == Integer.MAX_VALUE) {
@@ -61,8 +56,19 @@ public class Main {
                     System.out.println(distance[i]);
                 }
             }
-        } else {
+        } else { // 음의 사이클이 있을 때
             System.out.println(-1);
+        }
+    }
+    public static class Edge {
+        int start;
+        int end;
+        int weight;
+
+        public Edge(int start, int end, int weight) {
+            this.start = start;
+            this.end = end;
+            this.weight = weight;
         }
     }
 }
