6288b8b0471c4161ae470f4b9c92ae6f2ba3caff
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-10-10 11:17:04 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 84 ms, Memory: 12464 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -7,44 +7,38 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine()); // 수열의 크기
-        dp = new Integer[n];
-        arr = new int[n];
+        int N = Integer.parseInt(br.readLine()); // 수열 A의 크기
+        dp = new Integer[N];
+        arr = new int[N];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 0 부터 n - 1 까지 탐색
-        for (int i = 0; i < n; i++) {
-            getMax(i);
+        for (int i = 0; i < N; i++) {
+            LIS(i);
         }
 
-        // 저장된 dp 배열로 최댓값 구하기
-        int max = dp[0];
-        for (int i = 1; i < n; i++) {
-            max = Math.max(max, dp[i]);
+        int max = -1;
+        for (int i = 0; i < N; i++) {
+            max = Math.max(dp[i], max);
         }
 
         System.out.println(max);
+
     }
 
-    public static int getMax(int n) {
-
+    private static int LIS(int n) {
         if (dp[n] == null) {
-            dp[n] = 1; // 1로 초기화
-            
-            // n 이전의 노드들을 탐색
-            for (int i = n - 1; i >= 0; i--) {
-                // 이전의 노드 중 arr[n] 보다 작은 걸 발견하면, 
+            dp[n] = 1;
+
+            for (int i = n-1; i >= 0; i--) {
                 if (arr[i] < arr[n]) {
-                    // dp 배열 개수 비교 후 더 큰걸 dp[n]에 저장. 새로 저장할 땐 개수 ++
-                    dp[n] = Math.max(dp[n], getMax(i) + 1);
+                    dp[n] = Math.max(dp[n], LIS(i) + 1);
                 }
             }
         }
         return dp[n];
     }
-
 }
