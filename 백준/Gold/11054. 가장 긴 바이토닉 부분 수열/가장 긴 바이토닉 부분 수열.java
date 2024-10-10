f142b73f1830dae3e137d565e786d7947dcda35f
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-10-10 11:07:29 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 116 ms, Memory: 19776 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -1,26 +1,24 @@
 import java.io.*;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-
 public class Main {
-    static int[] arr;
     static Integer[] LIS_dp;
     static Integer[] LDS_dp;
+    static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
 
-        arr = new int[N];
         LIS_dp = new Integer[N];
         LDS_dp = new Integer[N];
+        arr = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // LIS, LDS 각각 수행
         for (int i = 0; i < N; i++) {
             LIS(i);
             LDS(i);
@@ -31,30 +29,32 @@ public class Main {
             max = Math.max(LIS_dp[i] + LDS_dp[i], max);
         }
 
-        System.out.println(max-1);
+        System.out.println(max - 1);
+
     }
 
-    public static int LIS(int n) {
+    private static int LIS(int n) { // 최장 증가 부분수열
         if (LIS_dp[n] == null) {
             LIS_dp[n] = 1;
 
+            // n 이전의 노드들을 탐색
             for (int i = n-1; i >= 0; i--) {
                 if (arr[i] < arr[n]) {
-                    LIS_dp[n] = Math.max(LIS_dp[n], LIS(i)+1);
+                    LIS_dp[n] = Math.max(LIS_dp[n], LIS(i) + 1);
                 }
             }
         }
         return LIS_dp[n];
     }
 
-    public static int LDS(int n) {
+    private static int LDS(int n) { // 최장 감소 부분수열
         if (LDS_dp[n] == null) {
             LDS_dp[n] = 1;
 
+            // n 이후의 노드들을 탐색
             for (int i = n+1; i < LDS_dp.length; i++) {
-                // 역방향으로 탐색
                 if (arr[i] < arr[n]) {
-                    LDS_dp[n] = Math.max(LDS_dp[n], LDS(i)+1);
+                    LDS_dp[n] = Math.max(LDS_dp[n], LDS(i) + 1);
                 }
             }
         }
