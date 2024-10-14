78cf6855582f147564f4587b47436b3ad9fdeedf
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-10-14 13:12:35 +0900

    [Silver I] Title: 트리 순회, Time: 64 ms, Memory: 11556 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,29 +1,30 @@
 import java.io.*;
-import java.util.*;
+
 
 public class Main {
     static int[][] tree;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+
+        int N = Integer.parseInt(br.readLine()); // 노드의 개수
         tree = new int[26][2];
+
         for (int i = 0; i < N; i++) {
             String[] temp = br.readLine().split(" ");
-            int parentNode = temp[0].charAt(0)-'A';
+            int node = temp[0].charAt(0) - 'A'; // index 변환을 위해 A 빼서 0 으로 바꿔주기
+            int left = temp[1].charAt(0);
+            int right = temp[2].charAt(0);
 
-            char leftNode = temp[1].charAt(0);
-            char rightNode = temp[2].charAt(0);
-
-            if (leftNode == '.') {
-                tree[parentNode][0] = -1;
+            if (left == '.') { // 자식 노드가 없으면(.이면) 대신 -1 저장하기
+                tree[node][0] = -1;
             } else {
-                tree[parentNode][0] = leftNode - 'A';
+                tree[node][0] = left - 'A'; // 있으면 index 변환 저장
             }
 
-            if (rightNode == '.') {
-                tree[parentNode][1] = -1;
+            if (right == '.') {
+                tree[node][1] = -1;
             } else {
-                tree[parentNode][1] = rightNode - 'A';
+                tree[node][1] = right - 'A';
             }
         }
 
@@ -34,31 +35,31 @@ public class Main {
         postOrder(0);
         System.out.println();
     }
-    public static void preOrder(int n) {
-        if (n == -1) {
+
+    private static void preOrder(int i) {
+        if (i == -1) {
             return;
         }
-        System.out.print((char) (n + 'A'));
-        preOrder(tree[n][0]);
-        preOrder(tree[n][1]);
+        System.out.print((char) (i + 'A')); // 현재 노드 출력
+        preOrder(tree[i][0]); // 왼쪽 탐색
+        preOrder(tree[i][1]); // 오른쪽 탐색
     }
 
-    public static void inOrder(int n) {
-        if (n == -1) {
+    private static void inOrder(int i) {
+        if (i == -1) {
             return;
         }
-        inOrder(tree[n][0]);
-        System.out.print((char) (n + 'A'));
-        inOrder(tree[n][1]);
+        inOrder(tree[i][0]);
+        System.out.print((char) (i + 'A'));
+        inOrder(tree[i][1]);
     }
 
-    public static void postOrder(int n) {
-        if (n == -1) {
+    private static void postOrder(int i) {
+        if (i == -1) {
             return;
         }
-        postOrder(tree[n][0]);
-        postOrder(tree[n][1]);
-        System.out.print((char) (n + 'A'));
+        postOrder(tree[i][0]);
+        postOrder(tree[i][1]);
+        System.out.print((char) (i + 'A'));
     }
 }
-
