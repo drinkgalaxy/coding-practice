c3b58028311215c09a7458ecdd957acb129b39ce
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-10-15 16:49:08 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.05 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[] arr) {
+        int temp = LCM(arr[0], arr[1]);
+        
+        for (int i = 2; i < arr.length; i++) {
+            temp = LCM(temp, arr[i]);
+        }
+        
+        return temp;
+    }
+    
+    public int GCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return GCD(b, a % b);
+        }
+    }
+    
+    public int LCM(int a, int b) {
+        return (a * b) / GCD(a, b);
+    }
+}
