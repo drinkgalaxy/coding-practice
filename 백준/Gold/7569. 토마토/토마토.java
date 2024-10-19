6145f4c6742a49e3c08196ecf43624623580a3e7
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-10-19 14:18:53 +0900

    [Gold V] Title: 토마토, Time: 504 ms, Memory: 127276 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -5,8 +5,7 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int[][][] tomato;
-    static boolean[][][] visited;
-    static int[] dx = {-1, 1, 0, 0, 0, 0}; // 위, 아래, 왼쪽, 오른쪽, 앞, 뒤
+    static int[] dx = {-1, 1, 0, 0, 0, 0}; // 좌, 우, 상, 하, 앞, 뒤
     static int[] dy = {0, 0, -1, 1, 0, 0};
     static int[] dh = {0, 0, 0, 0, 1, -1};
     static int M, N, H;
@@ -14,62 +13,57 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        M = Integer.parseInt(st.nextToken()); // 가로칸의 수 c
-        N = Integer.parseInt(st.nextToken()); // 세로칸의 수 r
-        H = Integer.parseInt(st.nextToken()); // 주어지는 높이
+        M = Integer.parseInt(st.nextToken()); // 가로칸의 수 = col
+        N = Integer.parseInt(st.nextToken()); // 세로칸의 수 = row
+        H = Integer.parseInt(st.nextToken()); // 쌓아올려지는 높이
 
         tomato = new int[H][N][M];
-        visited = new boolean[H][N][M];
 
-        // 토마토 입력 받기 및 익은 토마토를 큐에 추가
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < N; j++) {
+        for (int k = 0; k < H; k++) {
+            for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine(), " ");
-                for (int k = 0; k < M; k++) {
-                    tomato[i][j][k] = Integer.parseInt(st.nextToken());
-                    if (tomato[i][j][k] == 1) {
-                        queue.offer(new Node(i, j, k, 0));
+                for (int j = 0; j < M; j++) {
+                    tomato[k][i][j] = Integer.parseInt(st.nextToken());
+                    if (tomato[k][i][j] == 1) {
+                        queue.offer(new Node(k, i, j, 0));
                     }
                 }
             }
         }
 
-        // BFS 실행
-        bfs();
+        BFS();
     }
 
-    public static void bfs() {
+    public static void BFS() {
         int date = 0;
         while (!queue.isEmpty()) {
             Node n = queue.poll();
             date = n.date;
-            for (int j = 0; j < 6; j++) {
-                int nh = dh[j] + n.h;
-                int nx = dx[j] + n.x;
-                int ny = dy[j] + n.y;
-                if (nh >= 0 && nx >= 0 && ny >= 0 && nh < H && nx < N && ny < M) {
-                    if (tomato[nh][nx][ny] == 0) {
-                        tomato[nh][nx][ny] = 1;
-                        queue.offer(new Node(nh, nx, ny, date+1));
+            for (int i = 0; i < 6; i++) {
+                int h = dh[i] + n.h;
+                int x = dx[i] + n.x;
+                int y = dy[i] + n.y;
+                if (x < N && x >= 0 && y < M && y >= 0 && h < H && h >= 0) {
+                    if (tomato[h][x][y] == 0) {
+                        tomato[h][x][y] = 1;
+                        queue.offer(new Node(h, x, y, date + 1));
                     }
                 }
             }
         }
-
         if (check()) {
             System.out.println(date);
         } else {
             System.out.println(-1);
         }
+
     }
 
-    public static boolean check() {
-        for (int i = 0; i < H; i++) {
-            for (int j = 0; j < N; j++) {
-                for (int k = 0; k < M; k++) {
-                    // 안익은 토마토가 있으면
-                    if (tomato[i][j][k] == 0) {
+    private static boolean check() {
+        for (int k = 0; k < H; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (tomato[k][i][j] == 0) {
                         return false;
                     }
                 }
