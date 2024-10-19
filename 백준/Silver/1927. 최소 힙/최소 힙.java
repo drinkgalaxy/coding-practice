fd84182c8e4309c80a0fb03adf3b859db458f132
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-10-19 14:46:51 +0900

    [Silver II] Title: 최소 힙, Time: 240 ms, Memory: 24584 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                if (queue.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(queue.poll()).append("\n");
+                }
+            } else {
+                queue.offer(num);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
