107bf219008b8afd8f265db9a034613116eb0587
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-10-20 16:01:21 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 64 ms, Memory: 11860 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -1,76 +1,74 @@
 import java.io.*;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int count[] = new int[101];
-    static int ladderAndSnake[] = new int[101];
-    static boolean visited[] = new boolean[101];
+    static int[] game = new int[101];
+    static int[] count = new int[101];
+    static boolean[] visited = new boolean[101];
     static int N, M;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
         N = Integer.parseInt(st.nextToken()); // 사다리의 수
         M = Integer.parseInt(st.nextToken()); // 뱀의 수
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) { // 사다리
             st = new StringTokenizer(br.readLine(), " ");
-            // x번 칸에 도착하면, y번 칸으로 이동
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
-            ladderAndSnake[x] = y;
-
+            // x에 도착하면 y로 이동
+            game[x] = y;
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            // u번 칸에 도착하면, v번 칸으로 이동
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            ladderAndSnake[u] = v;
+            game[u] = v;
         }
 
-        bfs();
-
+        BFS();
     }
 
-    private static void bfs() {
+    private static void BFS() {
         Queue<Integer> queue = new LinkedList<>();
-        queue.offer(1); // 시작
-        count[1] = 0;
+        queue.offer(1); // 시작은 1에서부터
         visited[1] = true;
-
+        count[1] = 0;
+        
         while (!queue.isEmpty()) {
             int n = queue.poll();
 
             if (n == 100) {
-                System.out.println(count[n]); // 100 이면 횟수 바로 리턴
+                System.out.println(count[n]);
                 return;
             }
 
             for (int i = 1; i <= 6; i++) {
-                int x = n + i; // 1부터 6까지 이동
+                int x = n + i;
                 if (x > 100) {
-                    continue; // 더했을 때 100보다 크면 그 경우는 무시
-                }
-                if (visited[x]) { // 방문한 칸이여도 그 경우는 무시
                     continue;
                 }
-                visited[x] = true;
+                if (visited[x]) {
+                    continue;
+                }
 
-                if (ladderAndSnake[x] != 0) { // 사다리 또는 뱀위 위치일 때
-                    if (!visited[ladderAndSnake[x]]) {
-                        queue.offer(ladderAndSnake[x]);
-                        visited[ladderAndSnake[x]] = true;
-                        count[ladderAndSnake[x]] = count[n] + 1;
+                visited[x] = true;
+                if (game[x] != 0) { // 사다리나 뱀이 있는 칸이면
+                    if (!visited[game[x]]) {
+                        queue.offer(game[x]);
+                        visited[game[x]] = true;
+                        count[game[x]] = count[n] + 1;
                     }
-                } else { // 아무것도 아닐 때 그 칸으로 이동하여 큐에 추가
+                } else {
                     queue.offer(x);
                     visited[x] = true;
                     count[x] = count[n] + 1;
                 }
+
             }
         }
     }
-
 }
