9aa1427c3c1c1e38fdb3ab2325794e96dbc27cbb
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-10-21 10:39:23 +0900

    [Gold V] Title: 집합의 표현, Time: 1332 ms, Memory: 55224 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -3,29 +3,27 @@ import java.util.*;
 
 public class Main {
     static int[] parent;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        parent = new int[N+1];
 
-        // 모두 대표노드이므로 자신의 인덱스 값으로 초기화
-        for (int i = 0; i <= N; i++) {
-            parent[i] = i;
+        int n = Integer.parseInt(st.nextToken()); // 집합의 개수
+        int m = Integer.parseInt(st.nextToken()); // 연산의 개수
+
+        parent = new int[n+1];
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i; // 대표 노드를 자기 자신으로 초기화
         }
 
-        for (int i = 0; i < M; i++) {
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int calculation = Integer.parseInt(st.nextToken());
+            int question = Integer.parseInt(st.nextToken());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            if (calculation == 0) {
-                union(a, b);
+            if (question == 0) {
+                union(a, b); // 연산 합치기
             } else {
-                if (checkSum(a, b)) {
+                if (checkSame(a, b)) {
                     System.out.println("YES");
                 } else {
                     System.out.println("NO");
@@ -34,7 +32,13 @@ public class Main {
         }
     }
 
-    public static void union(int a, int b) {
+    private static boolean checkSame(int a, int b) {
+        a = find(a);
+        b = find(b);
+        return a == b;
+    }
+
+    private static void union(int a, int b) {
         a = find(a);
         b = find(b);
         if (a != b) {
@@ -42,21 +46,11 @@ public class Main {
         }
     }
 
-    public static int find(int a) {
+    private static int find(int a) {
         if (a == parent[a]) {
             return a;
         } else {
-            return parent[a] = find(parent[a]);
-        }
-    }
-
-    public static boolean checkSum(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (parent[a] == parent[b]) {
-            return true;
-        } else {
-            return false;
+            return parent[a] = find(parent[a]); // 재귀 함수 형태로 경로 압축
         }
     }
 }
