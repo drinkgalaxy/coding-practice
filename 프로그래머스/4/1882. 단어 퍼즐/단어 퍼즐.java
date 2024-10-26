64a26783da056ef6b612ff9371bcac7911e56ef2
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-10-26 19:44:21 +0900

    [level 4] Title: 단어 퍼즐, Time: 122.70 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/4/1882. 단어 퍼즐/단어 퍼즐.java
+++ 프로그래머스/4/1882. 단어 퍼즐/단어 퍼즐.java
@@ -1,24 +1,23 @@
 class Solution {
     public int solution(String[] strs, String t) {
-        int answer = 0;
-        int[] dp = new int[t.length() + 1];
         
-        dp[0] = 0;
+        int[] dp = new int[t.length()+1];
+        // dp 값 초기화
         for (int i = 1; i <= t.length(); i++) {
             dp[i] = Integer.MAX_VALUE;
         }
-        
-        for (int i = 0; i <= t.length(); i++) {
+        dp[0] = 0;
+        for (int i = 0; i < t.length(); i++) {
             if (dp[i] != Integer.MAX_VALUE) {
                 for (String str : strs) {
                     if (t.startsWith(str, i)) {
                         int nextIndex = i + str.length();
-                        dp[nextIndex] = Math.min(dp[nextIndex], dp[i] + 1);
+                        dp[nextIndex] = Math.min(dp[nextIndex], dp[i] +1);
                     }
                 }
             }
         }
 
-        return dp[t.length()] == Integer.MAX_VALUE ? -1 : dp[t.length()];
+        return (dp[t.length()] == Integer.MAX_VALUE) ? -1 : dp[t.length()];
     }
 }
