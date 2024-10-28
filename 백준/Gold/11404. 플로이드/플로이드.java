2a1a387da846cf92f3c4952d0a4b73ab7c804d01
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-10-28 13:37:19 +0900

    [Gold IV] Title: 플로이드, Time: 316 ms, Memory: 43556 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,39 +1,40 @@
 import java.io.*;
-import java.util.StringTokenizer;
-
+import java.util.*;
 
 public class Main {
-    static int[][] distance; // 인접 행렬 선언
+    static int[][] distance;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine()); // 도시의 개수
         int m = Integer.parseInt(br.readLine()); // 버스의 개수
+
         distance = new int[n+1][n+1];
 
         // 인접 행렬 초기화
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                if (i == j) { // 시작 도시와 출발 도시가 같으면 비용이 0
+                if (i == j) {
                     distance[i][j] = 0;
                 } else {
-                    distance[i][j] = 10000001;
+                    distance[i][j] = 987654321;
                 }
             }
         }
 
         for (int i = 0; i < m; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken()); // 버스의 시작 도시
-            int b = Integer.parseInt(st.nextToken()); // 버스의 도착 도시
-            int c = Integer.parseInt(st.nextToken()); // 한번 타는데 필요한 비용
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-            if (distance[a][b] > c) { // 주어진 버스 비용 정보를 인접 행렬에 저장
+            if (distance[a][b] > c) {
                 distance[a][b] = c;
             }
         }
 
-        // 플로이드 워셜 알고리즘 수행 (k가 가장 바깥쪽이 되야함.)
+        // 플로이드 워셜 알고리즘 수행
         for (int k = 1; k <= n; k++) {
             for (int i = 1; i <= n; i++) {
                 for (int j = 1; j <= n; j++) {
@@ -44,16 +45,18 @@ public class Main {
             }
         }
 
-        // 정답 배열 출력
+        // 정답 출력
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                if (distance[i][j] == 10000001) {
-                    System.out.print("0 ");
+                if (distance[i][j] == 987654321) {
+                    sb.append(0).append(" ");
                 } else {
-                    System.out.print(distance[i][j]+ " ");
+                    sb.append(distance[i][j]).append(" ");
                 }
             }
-            System.out.println();
+            sb.append("\n");
         }
+
+        System.out.println(sb);
     }
 }
