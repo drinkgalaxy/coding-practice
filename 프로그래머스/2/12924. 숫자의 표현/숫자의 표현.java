94d2763dae39bd00ea20577801d5ffc42360901c
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-10-29 11:36:04 +0900

    [level 2] Title: 숫자의 표현, Time: 1.33 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,12 +1,10 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0; 
-        
+        int answer = 0;
         for (int i = 1; i <= n; i++) {
             int sum = 0;
             for (int j = i; j <= n; j++) {
                 sum += j;
-                
                 if (sum >= n) {
                     if (sum == n) {
                         answer++;
