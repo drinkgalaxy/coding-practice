b82e637469f4e58597fa032962e943e817e17614
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-11-01 18:54:30 +0900

    [level 2] Title: 구명보트, Time: 14.51 ms, Memory: 56.4 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,21 +1,23 @@
 import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
-
         Arrays.sort(people);
-        
+
         int left = 0;
         int right = people.length-1;
-        int boat = 0;
         
+        int count = 0;
         while (left <= right) {
             if (people[left] + people[right] <= limit) {
                 left++;
+                right--;
+                count++;
+            } else {
+                right--;
+                count++;
             }
-            right--;
-            boat++;
         }
 
-        return boat;
+        return count;
     }
 }
