b9bb078bdea9dbb539286e07d0cddfe15eeec4b4
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-11-06 19:07:42 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.24 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,26 +1,30 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
-
+        int[] answer = new int[2];
         ArrayList<String> list = new ArrayList<>();
-        int count = 1;
+        
+        int turn = 1;
         int num = 1;
-        list.add(words[0]); // 첫 단어 미리 추가 
+        
+        list.add(words[0]); // num 1
         for (int i = 1; i < words.length; i++) {
             num = (num % n) + 1;
-            if (num == 1) { // 한바퀴 돌면
-                count++;
+            if (num == 1) {
+                turn++;
             }
-            
-            char pre = words[i-1].charAt(words[i-1].length()-1);
-            char now = words[i].charAt(0);
-            if (list.contains(words[i]) || pre != now) {
-                return new int[]{num, count};
+
+            char preWord = words[i-1].charAt(words[i-1].length()-1);
+            char afterWord = words[i].charAt(0);
+            if (list.contains(words[i]) || preWord != afterWord) {
+                answer[0] = num;
+                answer[1] = turn;
+                return answer;
             } else {
                 list.add(words[i]);
             }
         }
-    
+
         return new int[]{0, 0};
     }
 }
