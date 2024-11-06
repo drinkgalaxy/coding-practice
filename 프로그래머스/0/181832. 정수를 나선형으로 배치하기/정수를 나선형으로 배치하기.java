d9f0c538a58dd614bd172f1fc1e73ad54ba49115
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-11-06 18:31:48 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.12 ms, Memory: 65.3 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -1,18 +1,19 @@
 class Solution {
     public int[][] solution(int n) {
         int[][] answer = new int[n][n];
-        int[] dx = {1, 0, -1, 0};
+    
+        int[] dx = {1, 0, -1, 0}; // 시계방향 : 우, 하, 좌, 상
         int[] dy = {0, 1, 0, -1};
+        
         int num = 1;
         int dir = 0;
-        int x = 0; 
+        int x = 0;
         int y = 0;
         
         while (num <= n * n) {
-            if (x < n && y < n && x >= 0 && y >= 0 && answer[y][x] == 0) {
+            if (x >= 0 && x < n && y >= 0 && y < n && answer[y][x] == 0) {
                 answer[y][x] = num++;
             } else {
-                // 유효 x -> 값 되돌리기
                 x -= dx[dir];
                 y -= dy[dir];
                 dir = (dir + 1) % 4;
