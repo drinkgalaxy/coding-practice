955d90bc105292a0f79585b5590b65bfc07bb6d5
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-11-25 10:42:14 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.19 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        String[] words = s.split(" ", -1); 
+        
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            for (int j = 0; j < word.length(); j++) {
+                if (j % 2 == 0) {
+                    sb.append(Character.toUpperCase(word.charAt(j)));
+                } else {
+                    sb.append(Character.toLowerCase(word.charAt(j)));
+                }
+            }
+            if (i < words.length - 1) {
+                sb.append(" ");
+            }
+        }
+        
+        return sb.toString();
+    }
+}
