bffe60ce3c5e36eab20d3ec288c4b063a72569fd
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-11-26 10:55:26 +0900

    [level 1] Title: 최소직사각형, Time: 1.71 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,24 +1,17 @@
-import java.util.*;
-import java.io.*;
-
 class Solution {
     public int solution(int[][] sizes) {
         int maxWidth = 0;
         int maxHeight = 0;
         
         for (int i = 0; i < sizes.length; i++) {
-            // 명함의 가로 길이와 세로 길이를 비교하고, 더 큰 값을 가로길이로, 더 작은 값을 세로길이로 선택
-            int width = Math.max(sizes[i][0], sizes[i][1]);
-            int height = Math.min(sizes[i][0], sizes[i][1]);
+            int max = Math.max(sizes[i][0], sizes[i][1]);
+            int min = Math.min(sizes[i][0], sizes[i][1]);
             
-            if (width > maxWidth) {
-                maxWidth = width;
-            }
-            if (height > maxHeight) {
-                maxHeight = height;
-            }
+            maxWidth = Math.max(max, maxWidth);
+            maxHeight = Math.max(min, maxHeight);
         }
-        return maxWidth * maxHeight;
         
+        
+        return maxWidth * maxHeight;
     }
 }
