a1b9355c8e7d4fe485d2bb29639bc13967739e8a
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-11-26 10:32:11 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 8.46 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -1,18 +1,11 @@
-import java.util.*;
-
 class Solution {
     public int solution(String t, String p) {
         int answer = 0;
-        int pLength = p.length();
-        long pLong = Long.valueOf(p);
-        
-        for (int i = 0; i <= t.length() - pLength; i++) {
-            long tSub = Long.valueOf(t.substring(i, i+pLength));
-            if (tSub <= pLong) {
+        for (int i = 0; i < t.length()-(p.length()-1); i++) {
+            if (Long.parseLong(t.substring(i, i+p.length())) <= Long.parseLong(p)) {
                 answer++;
             }
         }
-        
         return answer;
     }
 }
