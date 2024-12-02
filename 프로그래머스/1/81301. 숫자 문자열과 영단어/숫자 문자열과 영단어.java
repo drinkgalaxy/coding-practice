c1138149ceb26278a9fd473e77e4d4c25a966fd3
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-12-02 11:51:39 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.06 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,8 +1,6 @@
 class Solution {
     public int solution(String s) {
-        int answer = 0;
         String[] alphabets = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        
         for (int i = 0; i < alphabets.length; i++) {
             s = s.replace(alphabets[i], String.valueOf(i));
         }
