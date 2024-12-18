3f9b55907b17e971558482868b0ffc60482340e6
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-12-18 23:46:19 +0900

    [Gold V] Title: 토마토, Time: 484 ms, Memory: 120888 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,27 +1,22 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     static int[][] tomato;
-    static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0}; // 좌우상하
-    static int[] dy = {0, 0, -1, 1};
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
     static int M, N;
     static Queue<Node> queue = new LinkedList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        M = Integer.parseInt(st.nextToken()); // 가로칸의 수 c
-        N = Integer.parseInt(st.nextToken()); // 세로칸의 수 r
+        M = Integer.parseInt(st.nextToken()); // col
+        N = Integer.parseInt(st.nextToken()); // row
         tomato = new int[N][M];
-        visited = new boolean[N][M];
 
-        // 토마토 입력 받기 및 익은 토마토를 큐에 추가
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 tomato[i][j] = Integer.parseInt(st.nextToken());
                 if (tomato[i][j] == 1) {
@@ -30,11 +25,10 @@ public class Main {
             }
         }
 
-        // BFS 실행
         bfs();
     }
 
-    public static void bfs() {
+    static public void bfs() {
         int date = 0;
         while (!queue.isEmpty()) {
             Node n = queue.poll();
@@ -43,10 +37,10 @@ public class Main {
             for (int i = 0; i < 4; i++) {
                 int nx = dx[i] + n.x;
                 int ny = dy[i] + n.y;
-                if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
                     if (tomato[nx][ny] == 0) {
                         tomato[nx][ny] = 1;
-                        queue.offer(new Node(nx, ny, date+1));
+                        queue.offer(new Node(nx, ny, date + 1));
                     }
                 }
             }
@@ -59,10 +53,9 @@ public class Main {
         }
     }
 
-    public static boolean check() {
+    static public boolean check() {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                // 안익은 토마토가 있으면
                 if (tomato[i][j] == 0) {
                     return false;
                 }
@@ -71,7 +64,7 @@ public class Main {
         return true;
     }
 
-    public static class Node {
+    static public class Node {
         int x, y, date;
 
         public Node(int x, int y, int date) {
