cb6e08c3ca1f8c40b9866a5f84d964a00de12868
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-12-19 12:22:08 +0900

    [level 1] Title: 모의고사, Time: 0.70 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,48 +1,52 @@
 import java.util.*;
-import java.io.*;
-
 class Solution {
-    public ArrayList<Integer> solution(int[] answers) {
-        int[] numbers = new int[3];
-        ArrayList<Integer> result = new ArrayList<>();
+    public int[] solution(int[] answers) {
         int[] number1 = {1, 2, 3, 4, 5};
         int[] number2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] number3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        numbers[0] = countAnswer(number1, answers);
-        numbers[1] = countAnswer(number2, answers);
-        numbers[2] = countAnswer(number3, answers);
-        
-        if (numbers[0] > numbers[1] && numbers[0] > numbers[2]) {
-            result.add(1);
-        } else if (numbers[1] > numbers[0] && numbers[1] > numbers[2]) {
-            result.add(2);
-        } else if (numbers[2] > numbers[0] && numbers[2] > numbers[1]) {
-            result.add(3);
-        } else if (numbers[0] == numbers[1] && numbers[0] > numbers[2]) {
-            result.add(1);
-            result.add(2);
-        } else if (numbers[1] == numbers[2] && numbers[1] > numbers[0]) {
-            result.add(2);
-            result.add(3);
-        } else if (numbers[2] == numbers[0] && numbers[2] > numbers[1]) {
-            result.add(1);
-            result.add(3);
-        } else {
-            result.add(1);
-            result.add(2);
-            result.add(3);
-        }
+        int[] numbers = new int[3];
+        numbers[0] = countAnswer(answers, number1);
+        numbers[1] = countAnswer(answers, number2);
+        numbers[2] = countAnswer(answers, number3);
 
+        ArrayList<Integer> resultList = new ArrayList<>();
+        if (numbers[0] == numbers[1] && numbers[1] > numbers[2]) {
+            resultList.add(1);
+            resultList.add(2);
+        } else if (numbers[0] == numbers[2] && numbers[2] > numbers[1]) {
+            resultList.add(1);
+            resultList.add(3);
+        } else if (numbers[1] == numbers[2] && numbers[2] > numbers[0]) {
+            resultList.add(2);
+            resultList.add(3);
+        } else if (numbers[0] > numbers[1] && numbers[0] > numbers[2]) {
+            resultList.add(1);
+        } else if (numbers[1] > numbers[0] && numbers[1] > numbers[2]) {
+            resultList.add(2);
+        } else if (numbers[2] > numbers[0] && numbers[2] > numbers[1]) {
+            resultList.add(3);
+        } else {
+            resultList.add(1);
+            resultList.add(2);
+            resultList.add(3);
+        }
+        
+        int[] result = new int[resultList.size()];
+        int idx = 0;
+        for (Integer i : resultList) {
+            result[idx++] = i;
+        }
+        
         return result;
     }
     
-    public static int countAnswer(int[] numbers, int[] answers) {
+    public static int countAnswer(int[] answers, int[] numbers) {
         int count = 0;
         for (int i = 0; i < answers.length; i++) {
             if (answers[i] == numbers[i % numbers.length]) {
                 count++;
             }
-        } 
+        }
         return count;
     }
 }
