203285e886c9142685872fff20445f4b31a0ea26
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-12-24 23:47:51 +0900

    [Gold IV] Title: 사이클 게임, Time: 624 ms, Memory: 135420 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.java
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -1,57 +1,59 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 
 public class Main {
-    static int[] parent;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+  static int[] parent;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        parent = new int[n];
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-        }
-
-        int result = 0;
-        for (int i = 1; i <= m; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            // 각각 i번째 차례에 플레이어가 선택한 두 점 a, b의 정보
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-
-            // 연결했을 때 마다 사이클이 생겼는지 확인
-            if (find(a) == find(b)) {
-                result = i;
-                break;
-            }
-            // 같은 집합이 아니면 합집합 수행
-            union(a, b);
-        }
-
-        System.out.println(result);
+    parent = new int[n+1];
+    for (int i = 1; i <= n; i++) {
+      parent[i] = i;
     }
 
-    static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a != b) {
-            if (a < b) {
-                parent[b] = a;
-            } else {
-                parent[a] = b;
-            }
-        }
-    }
+    int num = 0;
+    for (int i = 1; i <= m; i++) {
+      st = new StringTokenizer(br.readLine());
+      int x = Integer.parseInt(st.nextToken());
+      int y = Integer.parseInt(st.nextToken());
 
-    static int find(int a) {
-        if (a == parent[a]) {
-            return a;
-        } else {
-            return parent[a] = find(parent[a]);
-        }
+      if (find(x) == find(y)) {
+        num = i;
+        break;
+      } else {
+        union(x, y);
+      }
     }
+    System.out.println(num);
+  }
+
+  public static void union(int a, int b) {
+    a = find(a);
+    b = find(b);
+    if (a != b) {
+      if (a < b) {
+        parent[b] = a;
+      } else {
+        parent[a] = b;
+      }
+    }
+  }
+
+  public static int find(int a) {
+    if (a == parent[a]) {
+      return a;
+    } else {
+      return parent[a] = find(parent[a]);
+    }
+  }
 }
