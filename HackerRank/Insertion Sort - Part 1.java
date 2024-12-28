9c3c6b6a58cd0ba9783ab60bfbc416bb36f89f79
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-12-28 22:51:20 +0900

    Create Insertion Sort - Part 1.java

---
+++ HackerRank/Insertion Sort - Part 1.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'insertionSort1' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static void insertionSort1(int n, List<Integer> arr) {
+    // Write your code here
+        int index = 0;
+        int[] myarr = new int[n];
+        for (Integer i : arr) {
+            myarr[index++] = i;
+        }
+    
+        int value = myarr[n-1];
+        int i = n-2;
+        
+        while (i >= 0 && myarr[i]> value) {
+            myarr[i+1] = myarr[i]; 
+            printArray(myarr);
+            i--;
+        }
+        
+        myarr[i+1] = value;
+        printArray(myarr);
+    }
+    
+    public static void printArray(int[] arr) {
+        for (int a : arr) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.insertionSort1(n, arr);
+
+        bufferedReader.close();
+    }
+}
