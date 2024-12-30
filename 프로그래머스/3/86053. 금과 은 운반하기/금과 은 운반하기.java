2e72bacf49fef8b8232694ff90309e67c0d96206
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-12-30 20:43:42 +0900

    [level 3] Title: 금과 은 운반하기, Time: 157.83 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.java
+++ 프로그래머스/3/86053. 금과 은 운반하기/금과 은 운반하기.java
@@ -1,54 +1,52 @@
 class Solution {
     public long solution(int a, int b, int[] g, int[] s, int[] w, int[] t) {
-        long min = 1L;
-        // 최대 적재량이 1이고 편도 이동시간이 100000 일 때 시간 최대
-        // => (a+b)의 최댓값 * 왕복 최대 시간
-        long max = (2000000000) * 200000L;
+        long min = 0L;
+        long max = (2000000000L * 200000L);
         long mid;
         
         while (min < max) {
             mid = (min + max) / 2;
-            
-            if (IsPossible(mid, a, b, g, s, w, t)) { // 만족하면 더 작은 최소시간 시도
-                max = mid;
+            if (IsPossible(a, b, g, s, w, t, mid)) {
+                max = mid; // 최대를 --
             } else {
-                min = mid+1;
+                min = mid + 1; // 최소를 ++
             }
         }
+        
+        // 최소 시간을 구해야 하므로 max 리턴
         return max;
     }
     
-    boolean IsPossible(long time, int a, int b, int[] g, int[] s, int[] w, int[] t) {
-        long maxTotalG = 0L;
-        long maxTotalS = 0L;
-        long maxTotalGS = 0L;
-        long maxG, maxS, maxGS = 0L;
+    public static boolean IsPossible(int a, int b, int[] g, int[] s, int[] w, int[] t, long time) {
+        long allG = 0L;
+        long allS = 0L;
+        long allGS = 0L;
         
-        long count = 0L;
+        long G = 0L;
+        long S = 0L;
+        long GS = 0L;
+        
+        long count;
         for (int i = 0; i < g.length; i++) {
-            count = time / (t[i] * 2L);
-            if (time % (t[i] * 2L) >= t[i]) { // 편도로 한번 더!
+            count = time / (t[i] * 2L); // 왕복 이동 가능 횟수
+            if (time % (t[i] * 2L) >= t[i]) {
                 count++;
             }
             
-            maxG = Math.min(w[i] * count, g[i]);
-            maxS = Math.min(w[i] * count, s[i]);
-            maxGS = Math.min(w[i] * count, g[i] + s[i]);
+            G = Math.min(count * w[i], g[i]);
+            S = Math.min(count * w[i], s[i]);
+            GS = Math.min(count * w[i], g[i] + s[i]);
             
-            maxTotalG += maxG;
-            maxTotalS += maxS;
-            maxTotalGS += maxGS;
+            allG += G;
+            allS += S;
+            allGS += GS;
         }
-
         
-        // count 번 금 또는 은 옮겼는데 필요량 충족 X -> 시간 내에 못 옮김
-        if (maxTotalG < a || maxTotalS < b) {
+        if (allG < a || allS < b || allGS < a + b) {
             return false;
+        } else {
+            return true;
         }
-        // count 번 금과은 옮겼는데 필요량 충족 X -> 시간 내에 못 옮김
-        if (maxTotalGS < a+b) {
-            return false;
-        }
-        return true;
+       
     }
 }
