5850d4c291f14d6be8317d0f6370ada200d78b4b
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2024-12-31 17:04:49 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,32 +1,37 @@
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = 0;
+        int[] student = new int[n];
+        for (int i = 0; i < student.length; i++) {
+            student[i] = 1;
+        }
         
-        // 1. 새로운 student 배열을 만들고 lost, reserve의 정보를 각각 반영
-        // lost면 -1, reserve면 1
-        int[] student = new int[n+2];
-        for (int l : lost) {
-            student[l]--;
+        // lost check
+        for (int i = 0; i < lost.length; i++) {
+            student[lost[i]-1]--;
         }
-        for (int r : reserve) {
-            student[r]++;
+        
+        // reserve check
+        for (int i = 0; i < reserve.length; i++) {
+            student[reserve[i]-1]++;
         }
-        // 2. 배열의 값이 1인 인덱스를 보면서 앞뒤 인덱스의 배열의 값이 -1이면(lost 학생이면)
-        // 빌려주기 ++,--
-        for (int i = 1; i <= n; i++) {
-            if (student[i] == 1) {
-                if (student[i-1] == -1) {
+        
+        // student를 돌면서 해당 값이 2 이면 왼쪽 오른쪽 값이 0일 때 빌려주기
+        for (int i = 0; i < student.length; i++) {
+            if (student[i] == 2) {
+                if (i-1 >= 0 && student[i-1] == 0) {
                     student[i-1]++;
                     student[i]--;
-                } else if (student[i+1] == -1) {
+                } else if (i+1 < student.length && student[i+1] == 0) {
                     student[i+1]++;
                     student[i]--;
                 }
             }
         }
-        // 3. 인덱스의 배열의 값이 0 이상인 student 배열 개수 세기
-        for (int i = 1; i <= n; i++) {
-            if (student[i] >= 0) {
+        
+        // 최종적으로 체육복을 가지고 있는 student 수 리턴
+        int answer = 0;
+        for (int i = 0; i < student.length; i++) {
+            if (student[i] >= 1) {
                 answer++;
             }
         }
