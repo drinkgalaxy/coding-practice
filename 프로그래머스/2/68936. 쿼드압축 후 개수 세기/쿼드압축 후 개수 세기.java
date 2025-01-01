d612db4033569494caf70ab3a1012f19e80ebdf5
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2025-01-01 23:22:29 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 22.67 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,11 +1,14 @@
 class Solution {
-    static int[] answer = new int[2];
+    static int[] answer;
     public int[] solution(int[][] arr) {
+        answer = new int[2];
+        
         quad(arr, 0, 0, arr.length);
+        
         return answer;
     }
     
-    void quad(int[][] arr, int x, int y, int size) {
+    public static void quad(int[][] arr, int x, int y, int size) {
         if (IsPossible(arr, x, y, size, arr[x][y])) {
             if (arr[x][y] == 1) {
                 answer[1]++;
@@ -15,12 +18,13 @@ class Solution {
             return;
         }
         quad(arr, x, y, size / 2);
-        quad(arr, x, y + size / 2, size / 2);
         quad(arr, x + size / 2, y, size / 2);
+        quad(arr, x, y + size / 2, size / 2);
         quad(arr, x + size / 2, y + size / 2, size / 2);
+        
     }
     
-    boolean IsPossible(int[][] arr, int x, int y, int size, int val) {
+    public static boolean IsPossible(int[][] arr, int x, int y, int size, int val) {
         for (int i = x; i < x + size; i++) {
             for (int j = y; j < y + size; j++) {
                 if (arr[i][j] != val) {
