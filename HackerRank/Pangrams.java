7b02f62631e32c70c9bc93789ed1aead812decba
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2025-01-12 17:07:01 +0900

    Create Pangrams.java

---
+++ HackerRank/Pangrams.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String pangrams(String s) {
+    // Write your code here
+    String[] alphabet = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", 
+    "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    
+    Map<String, Integer> map = new HashMap<>();
+    for (String a : alphabet) {
+        map.put(a, 0);
+    }
+    
+    for (String str : s.split("")) {
+        if (str.equals(" ")) {
+            continue;
+        }
+        map.put(str.toLowerCase(), map.get(str.toLowerCase()) + 1);
+    }
+    
+    int sum = 0;
+    for (String m : map.keySet()) {
+        if (map.get(m) >= 1) {
+            sum++;
+        }
+    }
+    
+    return sum >= 26 ? "pangram" : "not pangram";
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.pangrams(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
