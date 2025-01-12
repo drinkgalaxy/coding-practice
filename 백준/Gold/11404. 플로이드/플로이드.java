28671582328aac9cf7df016328f3dc376425534b
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2025-01-12 16:03:21 +0900

    [Gold IV] Title: 플로이드, Time: 308 ms, Memory: 43700 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,62 +1,69 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 
 public class Main {
-    static int[][] distance;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine()); // 도시의 개수
-        int m = Integer.parseInt(br.readLine()); // 버스의 개수
 
-        distance = new int[n+1][n+1];
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 인접 행렬 초기화
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == j) {
-                    distance[i][j] = 0;
-                } else {
-                    distance[i][j] = 987654321;
-                }
-            }
+    int n = Integer.parseInt(br.readLine());
+    int m = Integer.parseInt(br.readLine());
+
+    int[][] distance = new int[n+1][n+1];
+    final int INF = 987654321;
+
+    // 인접 행렬 초기화
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n; j++) {
+        if (i == j) {
+          distance[i][j] = 0;
+        } else {
+          distance[i][j] = INF;
         }
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-
-            if (distance[a][b] > c) {
-                distance[a][b] = c;
-            }
-        }
-
-        // 플로이드 워셜 알고리즘 수행
-        for (int k = 1; k <= n; k++) {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    if (distance[i][j] > distance[i][k] + distance[k][j]) {
-                        distance[i][j] = distance[i][k] + distance[k][j];
-                    }
-                }
-            }
-        }
-
-        // 정답 출력
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (distance[i][j] == 987654321) {
-                    sb.append(0).append(" ");
-                } else {
-                    sb.append(distance[i][j]).append(" ");
-                }
-            }
-            sb.append("\n");
-        }
-
-        System.out.println(sb);
+      }
     }
+
+    for (int i = 1; i <= m; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
+      int c = Integer.parseInt(st.nextToken());
+
+      if (distance[a][b] > c) {
+        distance[a][b] = c;
+      }
+    }
+
+    // 플로이드 워셜 알고리즘 수행
+    for (int k = 1; k <= n; k++) {
+      for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+          if (distance[i][j] > distance[i][k] + distance[k][j]) {
+            distance[i][j] = distance[i][k] + distance[k][j];
+          }
+        }
+      }
+    }
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n; j++) {
+        if (distance[i][j] == INF) {
+          sb.append(0).append(" ");
+        } else {
+          sb.append(distance[i][j]).append(" ");
+        }
+      }
+      sb.append("\n");
+    }
+    System.out.println(sb);
+  }
 }
