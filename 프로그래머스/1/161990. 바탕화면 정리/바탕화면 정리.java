281f8281d453cca4b460977490573f6c5840941f
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2025-01-14 23:50:48 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.21 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,25 +1,21 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
-        // 정답 배열에 시작점, 종료점 값 초기화
-        int[] answer = {
-            Integer.MAX_VALUE,
-            Integer.MAX_VALUE,
-            Integer.MIN_VALUE,
-            Integer.MIN_VALUE
-        };
+        int lux = Integer.MAX_VALUE;
+        int luy = Integer.MAX_VALUE;
+        int rdx = Integer.MIN_VALUE;
+        int rdy = Integer.MIN_VALUE;
         
-         // # 을 탐색하면서 가장 작은 시작점, 가장 큰 종료점 정하기
         for (int i = 0; i < wallpaper.length; i++) {
             for (int j = 0; j < wallpaper[0].length(); j++) {
                 if (wallpaper[i].charAt(j) == '#') {
-                    answer[0] = Math.min(i, answer[0]);
-                    answer[1] = Math.min(j, answer[1]);
-                    answer[2] = Math.max(i+1, answer[2]);
-                    answer[3] = Math.max(j+1, answer[3]);
+                    lux = Math.min(lux, i);
+                    luy = Math.min(luy, j);
+                    rdx = Math.max(rdx, i+1);
+                    rdy = Math.max(rdy, j+1);
                 }
             }
         }
-    
-        return answer;
+        
+        return new int[]{lux, luy, rdx, rdy};
     }
 }
