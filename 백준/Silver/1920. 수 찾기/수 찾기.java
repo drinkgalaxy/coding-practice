44c332f913b04dce50bd1cc5ca6b7a3ea58f6250
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2025-01-14 19:54:50 +0900

    [Silver IV] Title: 수 찾기, Time: 440 ms, Memory: 54188 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,52 +1,59 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
+
 public class Main {
+  static int[] arr;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+    int N = Integer.parseInt(br.readLine());
+    arr = new int[N];
 
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(arr);
-
-        int M = Integer.parseInt(br.readLine());
-
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < M; i++) {
-            int target = Integer.parseInt(st.nextToken());
-
-            int l = 0;
-            int r = arr.length-1;
-            boolean find = false;
-
-            while (l <= r) {
-                int midIndex = (l + r) / 2;
-
-                if (arr[midIndex] < target) {
-                    l = midIndex+1;
-                } else if (target < arr[midIndex]) {
-                    r = midIndex-1;
-                } else {
-                    find = true;
-                    break;
-                }
-            }
-
-            if (find) {
-                sb.append(1).append("\n");
-            } else {
-                sb.append(0).append("\n");
-            }
-        }
-        System.out.println(sb);
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
     }
+
+    Arrays.sort(arr);
+
+    int M = Integer.parseInt(br.readLine());
+    int[] mrr = new int[M];
+
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < M; i++) {
+      mrr[i] = Integer.parseInt(st.nextToken());
+      if (binarySearch(mrr[i])) {
+        sb.append(1).append("\n");
+      } else {
+        sb.append(0).append("\n");
+      }
+    }
+
+    System.out.println(sb);
+  }
+
+  public static boolean binarySearch(int target) {
+    int lo = 0;
+    int hi = arr.length-1;
+    while (lo <= hi) {
+      int mid = (lo + hi) / 2;
+      if (arr[mid] < target) {
+        lo = mid + 1;
+      } else if (arr[mid] > target) {
+        hi = mid - 1;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
 }
