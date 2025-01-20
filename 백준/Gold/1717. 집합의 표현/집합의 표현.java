fd5a648c8d1a2101cdba29c335e3afac95099e1c
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2025-01-20 20:30:01 +0900

    [Gold V] Title: 집합의 표현, Time: 344 ms, Memory: 53220 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -1,56 +1,69 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 
 public class Main {
-    static int[] parent;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+  static int[] parent;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(st.nextToken()); // 집합의 개수
-        int m = Integer.parseInt(st.nextToken()); // 연산의 개수
+    int n = Integer.parseInt(st.nextToken()); // 집합의 개수
+    int m = Integer.parseInt(st.nextToken()); // 연산의 개수
 
-        parent = new int[n+1];
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i; // 대표 노드를 자기 자신으로 초기화
-        }
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int question = Integer.parseInt(st.nextToken());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            if (question == 0) {
-                union(a, b); // 연산 합치기
-            } else {
-                if (checkSame(a, b)) {
-                    System.out.println("YES");
-                } else {
-                    System.out.println("NO");
-                }
-            }
-        }
+    parent = new int[n+1];
+    for (int i = 0; i <= n; i++) {
+      parent[i] = i;
     }
 
-    private static boolean checkSame(int a, int b) {
-        a = find(a);
-        b = find(b);
-        return a == b;
-    }
+    for (int i = 0; i < m; i++) {
+      st = new StringTokenizer(br.readLine());
+      int x = Integer.parseInt(st.nextToken());
+      int a = Integer.parseInt(st.nextToken());
+      int b = Integer.parseInt(st.nextToken());
 
-    private static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a != b) {
-            parent[b] = a;
-        }
-    }
-
-    private static int find(int a) {
-        if (a == parent[a]) {
-            return a;
+      if (x == 1) {
+        if (find(a) == find(b)) {
+          sb.append("YES").append("\n");
         } else {
-            return parent[a] = find(parent[a]); // 재귀 함수 형태로 경로 압축
+          sb.append("NO").append("\n");
         }
+      } else {
+        union(a, b);
+      }
     }
+
+    System.out.println(sb);
+  }
+
+  private static int find(int a) {
+    if (a == parent[a]) {
+      return a;
+    } else {
+      return parent[a] = find(parent[a]);
+    }
+  }
+
+  private static void union(int a, int b) {
+    int pa = find(a);
+    int pb = find(b);
+    
+    if (pa != pb) {
+      if (pa < pb) {
+        parent[pb] = pa;
+      } else {
+        parent[pa] = pb;
+      }
+    }
+  }
 }
