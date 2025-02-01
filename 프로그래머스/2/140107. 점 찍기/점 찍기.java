e4a0f811d0d4c08e3eda82e21dd3015118df1886
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2025-02-01 23:50:39 +0900

    [level 2] Title: 점 찍기, Time: 15.93 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -1,7 +1,8 @@
+import java.util.*;
 class Solution {
     public long solution(int k, int d) {
         long answer = 0;
-        // 공식 x^2 + y^2 <= d^2
+        // x^2 + y^2 <= d^2
         // y <= d^2 - x^2 의 제곱근
         for (int x = 0; x <= d; x += k) {
             double y = Math.sqrt(Math.pow(d, 2) - Math.pow(x, 2));
