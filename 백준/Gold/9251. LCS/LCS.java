773b527d155bcf6dbcf2ee22a867c061e26ec8a4
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2025-02-04 17:35:25 +0900

    [Gold V] Title: LCS, Time: 144 ms, Memory: 25720 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,39 +1,48 @@
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 
 public class Main {
-    static char[] str1;
-    static char[] str2;
-    static Integer[][] dp;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static char[] str1;
+  static char[] str2;
+  static Integer[][] dp;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        str1 = br.readLine().toCharArray();
-        str2 = br.readLine().toCharArray();
+    str1 = br.readLine().toCharArray();
+    str2 = br.readLine().toCharArray();
 
-        dp = new Integer[str1.length][str2.length];
+    dp = new Integer[str1.length][str2.length];
 
-        System.out.println(LCS(str1.length - 1, str2.length - 1));
+    System.out.println(LCS(str1.length-1, str2.length-1));
+  }
+
+  private static int LCS(int x, int y) {
+    if (x == -1 || y == -1) {
+      return 0;
     }
 
-    private static int LCS(int x, int y) {
-        // 인덱스 밖 값일 경우 0 반환
-        if (x == -1 || y == -1) {
-            return 0;
-        }
+    if (dp[x][y] == null) {
+      dp[x][y] = 0;
 
-        if (dp[x][y] == null) {
-            dp[x][y] = 0; // 초기화
-
-            // str1 의 x번째 문자와 str2 의 y 번째 문자가 같은지 검사
-            if (str1[x] == str2[y]) {
-                dp[x][y] = LCS(x-1, y-1) + 1;
-            } else {
-                // 같지 않다면 더 큰 값 저장 
-                dp[x][y] = Math.max(LCS(x, y-1), LCS(x-1, y));
-            }
-        }
-        return dp[x][y];
+      if (str1[x] == str2[y]) {
+        dp[x][y] = LCS(x-1, y-1) + 1;
+      } else {
+        dp[x][y] = Math.max(LCS(x-1, y), LCS(x, y-1));
+      }
     }
+
+    return dp[x][y];
+  }
 }
+ 
