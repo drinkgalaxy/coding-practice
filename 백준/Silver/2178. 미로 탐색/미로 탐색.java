854c4b36a2e9bba770c76052cb3aa186e7ae56f3
HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-02-10 22:31:48 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 12312 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,69 +1,78 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
+import org.w3c.dom.Node;
 
 
 public class Main {
-    static int[] dx = {0, 0, -1, 1}; // 상하좌우
-    static int[] dy = {-1, 1, 0, 0};
-    static char[][] miro;
-    static int[][] dist;
-    static int N, M;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        dist = new int[N][M];
-        miro = new char[N][M];
+  static int N, M;
+  static char[][] miro;
+  static int[][] distance;
+  static int[] dx = {0, 0, -1, 1};
+  static int[] dy = {1, -1, 0, 0};
 
-        for (int i = 0; i < N; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < M; j++) {
-                miro[i][j] = line.charAt(j);
-                dist[i][j] = -1; // 방문 여부 초기화
-            }
-        }
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-        dist[0][0] = 1; // 출발 지점
-        BFS(0, 0);
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
 
-        System.out.println(dist[N-1][M-1]);
+    miro = new char[N][M];
+    distance = new int[N][M];
 
+    for (int i = 0; i < N; i++) {
+      String line = br.readLine();
+      for (int j = 0; j < M; j++) {
+        miro[i][j] = line.charAt(j);
+        distance[i][j] = -1;
+      }
     }
 
-    private static void BFS(int x, int y) {
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(new Node(x, y));
+    distance[0][0] = 1;
+    bfs(0, 0);
 
-        while (!queue.isEmpty()) {
-            Node n = queue.poll();
+    System.out.println(distance[N-1][M-1]);
+  }
 
-            for (int i = 0; i < 4; i++) {
-                int nx = dx[i] + n.s;
-                int ny = dy[i] + n.e;
+  private static void bfs(int x, int y) {
+    Queue<Node> queue = new LinkedList<>();
+    queue.offer(new Node(x, y));
 
-                if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
-                    if (dist[nx][ny] == -1 && miro[nx][ny] == '1') {
-                        dist[nx][ny] = dist[n.s][n.e] + 1;
-                        queue.offer(new Node(nx, ny));
-                    }
-                }
-            }
-
+    while (!queue.isEmpty()) {
+      Node now = queue.poll();
+      for (int i = 0; i < 4; i++) {
+        int nx = now.x + dx[i];
+        int ny = now.y + dy[i];
+        if (nx < N && nx >= 0 && ny < M && ny >= 0) {
+          if (miro[nx][ny] == '1' && distance[nx][ny] == -1) {
+            queue.offer(new Node(nx, ny));
+            distance[nx][ny] = distance[now.x][now.y] + 1;
+          }
         }
+      }
     }
+  }
 
-    public static class Node {
-        int s;
-        int e;
+  private static class Node {
 
-        public Node(int s, int e) {
-            this.s = s;
-            this.e = e;
-        }
+    int x;
+    int y;
+
+    public Node(int x, int y) {
+      this.x = x;
+      this.y = y;
     }
+  }
 }
+ 
