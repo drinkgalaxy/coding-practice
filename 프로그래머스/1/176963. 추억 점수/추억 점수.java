574fd11da84538af8671e859c90d2ee821956c52
HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-02-13 00:03:04 +0900

    [level 1] Title: 추억 점수, Time: 2.92 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,24 +1,21 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
-        // 1. 이름별 그리움 점수를 저장하는 HashMap 생성
         Map<String, Integer> map = new HashMap<>();
+        
         for (int i = 0; i < name.length; i++) {
             map.put(name[i], yearning[i]);
         }
-
-        // 2. 사진별 추억 점수 계산
+        
         int[] answer = new int[photo.length];
-
         for (int i = 0; i < photo.length; i++) {
             int sum = 0;
-            for (String person : photo[i]) {
-                sum += map.getOrDefault(person, 0); // 그리움 점수가 없으면 0
+            for (int j = 0; j < photo[i].length; j++) {
+                sum += map.getOrDefault(photo[i][j], 0);
             }
             answer[i] = sum;
         }
-
+        
         return answer;
     }
 }
