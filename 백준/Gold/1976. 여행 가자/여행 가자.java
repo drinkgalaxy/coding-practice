ff96d3ded5792e4cf62f37d88a77fd6e1f014c7f
HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-03-06 00:13:20 +0900

    [Gold IV] Title: 여행 가자, Time: 108 ms, Memory: 15916 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.java
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -1,66 +1,81 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+import org.w3c.dom.Node;
+
 
 public class Main {
-    static int[] parent;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+  static int[] parent;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine()); // 도시의 수
-        int M = Integer.parseInt(br.readLine()); // 여행 계획 도시들의 수
-        parent = new int[N+1];
+    int N = Integer.parseInt(br.readLine());
+    int M = Integer.parseInt(br.readLine());
 
-        // 초기화
-        for (int i = 0; i <= N; i++) {
-            parent[i] = i;
-        }
+    parent = new int[N+1];
 
-        for (int i = 1; i <= N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 1; j <= N; j++) {
-                int q = Integer.parseInt(st.nextToken());
-                if (q == 1) {
-                    union(i, j);
-                }
-            }
-        }
-
-        st = new StringTokenizer(br.readLine(), " ");
-        int root = find(Integer.parseInt(st.nextToken()));
-        boolean check = true;
-        for (int i = 1; i < M; i++) {
-            int q = Integer.parseInt(st.nextToken());
-            if (root != find(q)) {
-                check = false;
-                break;
-            }
-        }
-
-        if (check) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO");
-        }
+    // 초기화
+    for (int i = 0; i <= N; i++) {
+      parent[i] = i;
     }
 
-    static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-        if (a != b) {
-            if (a < b) {
-                parent[b] = a;
-            } else {
-                parent[a] = b;
-            }
+    for (int x = 1; x <= N; x++) {
+      st = new StringTokenizer(br.readLine());
+      for (int y = 1; y <= N; y++) {
+        int i = Integer.parseInt(st.nextToken());
+        if (i == 1) {
+          union(x, y);
         }
+      }
     }
 
-    static int find(int a) {
-        if (a == parent[a]) {
-            return a;
-        } else {
-            return parent[a] = find(parent[a]);
-        }
+    st = new StringTokenizer(br.readLine());
+    int root = find(Integer.parseInt(st.nextToken()));
+    boolean check = true;
+    for (int x = 1; x < M; x++) {
+      int i = Integer.parseInt(st.nextToken());
+      if (root != find(i)) {
+        check = false;
+        break;
+      }
     }
+
+    if (check) {
+      System.out.println("YES");
+    } else {
+      System.out.println("NO");
+    }
+
+  }
+
+  static void union(int a, int b) {
+    a = find(a);
+    b = find(b);
+    if (a != b) {
+      if (a < b) {
+        parent[b] = a;
+      } else {
+        parent[a] = b;
+      }
+    }
+  }
+
+  static int find(int a) {
+    if (a == parent[a]) {
+      return a;
+    } else {
+      return parent[a] = find(parent[a]);
+    }
+  }
 }
+ 
