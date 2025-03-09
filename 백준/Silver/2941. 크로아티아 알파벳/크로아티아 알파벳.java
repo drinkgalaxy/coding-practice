85abfcc08a665cdc7067c1d8be679eac4785d98f
HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-03-09 22:40:51 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 68 ms, Memory: 11652 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,44 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        ArrayList<String> strList = new ArrayList<>();
-        for (String str : s.split("")) {
-            strList.add(str);
-        }
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int count = 0;
-        for (int i = 0; i < strList.size(); i++) {
-            if (i < strList.size() - 1 && strList.get(i).equals("c") && (strList.get(i + 1).equals("=") || strList.get(i + 1).equals("-"))) {
-                count++;
-                i++;
-            } else if (i < strList.size() - 2 && strList.get(i).equals("d") && strList.get(i + 1).equals("z") && strList.get(i + 2).equals("=")) {
-                count++;
-                i += 2;
-            } else if (i < strList.size() - 1 && strList.get(i).equals("d") && strList.get(i + 1).equals("-")) {
-                count++;
-                i++;
-            } else if (i < strList.size() - 1 && strList.get(i).equals("l") && strList.get(i + 1).equals("j")) {
-                count++;
-                i++;
-            } else if (i < strList.size() - 1 && strList.get(i).equals("n") && strList.get(i + 1).equals("j")) {
-                count++;
-                i++;
-            } else if (i < strList.size() - 1 && strList.get(i).equals("s") && strList.get(i + 1).equals("=")) {
-                count++;
-                i++;
-            } else if (i < strList.size() - 1 && strList.get(i).equals("z") && strList.get(i + 1).equals("=")) {
-                count++;
-                i++;
-            } else {
-                count++;
-            }
-        }
-        System.out.println(count);
+    String[] alphabet = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+    String line = br.readLine();
+
+    for (int i = 0; i < alphabet.length; i++) {
+      if (line.contains(alphabet[i])) {
+        line = line.replaceAll(alphabet[i], " ");
+      }
     }
+
+    System.out.println(line.length());
+  }
 }
+ 
