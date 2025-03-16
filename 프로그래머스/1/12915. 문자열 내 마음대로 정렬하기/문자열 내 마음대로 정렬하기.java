7bcdf1e2097dcb74bc40166f21e92aac93cd733e
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-03-16 14:33:27 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 22.32 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,19 +1,18 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] strings, int n) {
-        String[] answer = new String[strings.length];
         List<String> list = new ArrayList<>();
-        
         for (int i = 0; i < strings.length; i++) {
             list.add(strings[i].charAt(n) + strings[i]);
         }
         
         Collections.sort(list);
         
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i).substring(1);
+        String[] arr = new String[list.size()];
+        int idx = 0;
+        for (String s : list) {
+            arr[idx++] = s.substring(1, s.length());
         }
-        
-        return answer;
+        return arr;
     }
 }
