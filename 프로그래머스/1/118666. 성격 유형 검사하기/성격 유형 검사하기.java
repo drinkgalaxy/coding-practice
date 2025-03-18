4b86b573b63ac5f7ea89b60cde63c980d0d5d41f
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-03-18 14:51:17 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 1.27 ms, Memory: 92.8 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -2,10 +2,8 @@ import java.util.*;
 class Solution {
     static Map<Character, Integer> map = new HashMap<>();
     public String solution(String[] survey, int[] choices) {
-        StringBuilder sb = new StringBuilder();
         
-         // 모든 유형의 키를 미리 초기화 
-        char[] types = {'R', 'T', 'C', 'F', 'J', 'M', 'A', 'N'};
+        char[] types = {'R', 'T', 'C', 'F', 'J', 'F', 'J', 'M', 'A', 'N'};
         for (char t : types) {
             map.put(t, 0);
         }
@@ -14,44 +12,42 @@ class Solution {
             saveScore(survey[i].charAt(0), survey[i].charAt(1), choices[i]);
         }
         
-        char[][] indicators = {{'R', 'T'}, {'C', 'F'}, {'J', 'M'}, {'A', 'N'}};
-        
-        for (char[] pair : indicators) {
-            if (compare(map.get(pair[0]), map.get(pair[1]))) {
-                sb.append(pair[0]);
-            } else {
-                sb.append(pair[1]);
-            }
+        char[][] arr = {{'R', 'T'}, {'C', 'F'}, {'J', 'M'}, {'A', 'N'}};
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            result.append(compare(arr[i][0], arr[i][1]));
         }
-        
-        return sb.toString();
+    
+        return result.toString();
     }
     
-    public static void saveScore(char type1, char type2, Integer score) {
-        if (score == 1) {
+    private static Character compare(char type1, char type2) {
+        if (map.get(type1) < map.get(type2)) {
+            return type2;
+        } else if (map.get(type1) > map.get(type2)) {
+            return type1;
+        } else {
+            return type1;
+        }
+    }
+    
+    private static void saveScore(char type1, char type2, int choice) {
+        if (choice == 1) {
             saveMap(type1, 3);
-        } else if (score == 2) {
+        } else if (choice == 2) {
             saveMap(type1, 2);
-        } else if (score == 3) {
+        } else if (choice == 3) {
             saveMap(type1, 1);
-        } else if (score == 5) {
+        } else if (choice == 5) {
             saveMap(type2, 1);
-        } else if (score == 6) {
+        } else if (choice == 6) {
             saveMap(type2, 2);
-        } else if (score == 7) {
+        } else if (choice == 7) {
             saveMap(type2, 3);
         }
     }
     
-    public static void saveMap(char type, Integer plus) {
-        map.put(type, map.get(type) + plus);
-    }
-    
-    public static boolean compare(Integer type1, Integer type2) {
-        if (type1 >= type2) {
-            return true;
-        } else {
-            return false;
-        }
+    private static void saveMap(char type, int score) {
+        map.put(type, map.get(type) + score);
     }
 }
