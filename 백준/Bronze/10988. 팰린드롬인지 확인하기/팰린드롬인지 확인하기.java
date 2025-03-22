2a94cb582a5ba2a11ea63c948ae6eb88236bb336
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-03-22 21:11:36 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 64 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -2,29 +2,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        String[] strArray = new String[str.length()];
-        int index = 0;
-        for (String s : str.split("")) {
-            strArray[index] = s;
-            index++;
-        }
+  static String word;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int count = 0;
-        for (int i = 0; i < strArray.length/2; i++) {
-            if (!strArray[i].equals(strArray[(strArray.length-1) - i])) {
-                count++;
-            }
-        }
-        if (count == 0) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
-        }
+    word = br.readLine();
 
+    System.out.println(check() ? 1 : 0);
+
+  }
+
+  private static boolean check() {
+    int s = 0;
+    int e = word.length()-1;
+
+    while (s < e) {
+      if (word.charAt(s) != word.charAt(e)) {
+        return false;
+      }
+      s++;
+      e--;
     }
+    return true;
+  }
 }
