3f42db3aafb3723890a169e45da4a8b8d7763c13
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-03-25 09:48:38 +0900

    [level 1] Title: 문자열 나누기, Time: 0.99 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -2,25 +2,24 @@ class Solution {
     public int solution(String s) {
         int answer = 0;
         int idx = 0;
-        
+        StringBuilder sb = new StringBuilder();
         while (idx < s.length()) {
-            char x = s.charAt(idx);
-            int xCount = 0;
-            int otherCount = 0;
+            char c = s.charAt(idx);
+            int isX = 0;
+            int isNotX = 0;
             
             while (idx < s.length()) {
-                if (s.charAt(idx) == x) {
-                    xCount++;
+                if (s.charAt(idx) == c) {
+                    isX++;
                 } else {
-                    otherCount++;
+                    isNotX++;
                 }
                 idx++;
                 
-                if (xCount == otherCount) {
+                if (isX == isNotX) {
                     break;
                 }
             }
-            
             answer++;
         }
         
