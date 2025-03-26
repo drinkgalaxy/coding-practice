dc1dd064d203b0b10b6052376543fdcf0ac7b10e
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-03-26 18:09:07 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,21 +1,19 @@
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
+        
+        // student 배열 초기화하고 상태 저장
         int[] student = new int[n];
         for (int i = 0; i < student.length; i++) {
             student[i] = 1;
         }
-        
-        // lost check
-        for (int i = 0; i < lost.length; i++) {
-            student[lost[i]-1]--;
+        for (int i : lost) {
+            student[i-1] -= 1;
+        }
+        for (int i : reserve) {
+            student[i-1] += 1;
         }
         
-        // reserve check
-        for (int i = 0; i < reserve.length; i++) {
-            student[reserve[i]-1]++;
-        }
-        
-        // student를 돌면서 해당 값이 2 이면 왼쪽 오른쪽 값이 0일 때 빌려주기
+        // 배열 돌면서 2인 애들 체크하고 주변에 0인 애 있으면 나눠주기
         for (int i = 0; i < student.length; i++) {
             if (student[i] == 2) {
                 if (i-1 >= 0 && student[i-1] == 0) {
@@ -28,14 +26,13 @@ class Solution {
             }
         }
         
-        // 최종적으로 체육복을 가지고 있는 student 수 리턴
-        int answer = 0;
+        int count = 0;
         for (int i = 0; i < student.length; i++) {
             if (student[i] >= 1) {
-                answer++;
+                count++;
             }
         }
         
-        return answer;
+        return count;
     }
 }
