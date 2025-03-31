a244464eb10eda14e201d7bea778a14b78746503
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-03-31 15:49:15 +0900

    [Silver IV] Title: 카드2, Time: 172 ms, Memory: 45588 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,24 +1,27 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Queue<Integer> queue = new LinkedList<>();
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+    int N = Integer.parseInt(br.readLine());
 
-        for (int i = 1; i <= N; i++) {
-            queue.add(i);
-        }
-        for (int i = 0; i < N-1; i++) {
-            if (!queue.isEmpty()) {
-                queue.poll();
-                int front = queue.peek();
-                queue.add(front);
-                queue.remove();
-            }
-        }
-        System.out.println(queue.peek());
+    Queue<Integer> queue = new LinkedList<>();
+
+    for (int i = 1; i <= N; i++) {
+      queue.add(i);
     }
+
+    while (queue.size() > 1) {
+      queue.poll();
+      queue.add(queue.poll());
+    }
+
+    System.out.println(queue.poll());
+
+  }
 }
