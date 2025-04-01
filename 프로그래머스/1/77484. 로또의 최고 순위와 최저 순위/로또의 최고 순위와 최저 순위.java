84a3230324e1c6fa8d3e93e0d707fac52e9e26a8
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-04-02 00:28:00 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,42 +1,34 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         
-        Set<Integer> set = new HashSet<>();
-        for (int i : win_nums) {
-            set.add(i);
-        }
-        
-        int yn = 0;
-        int zn = 0;
+        int[] arr = new int[46];
         
         for (int i : lottos) {
-            if (set.contains(i)) {
-                yn++;
-            } else if (i == 0) {
-                zn++;
+            arr[i]++;
+        }
+        
+        for (int i : win_nums) {
+            arr[i]++;
+        }
+        
+        int max = 0;
+        int min = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] == 2) {
+                min++;
             }
         }
+        max = min + arr[0];
         
-        int maxCount = yn + zn;
-        int minCount = yn;
-        
-        return new int[]{getCount(maxCount), getCount(minCount)};
+        return new int[]{score(max), score(min)};
     }
     
-    public int getCount(int count) {
-        if (count == 6) {
-            return 1;
-        } else if (count == 5) {
-            return 2;
-        } else if (count == 4) {
-            return 3;
-        } else if (count == 3) {
-            return 4;
-        } else if (count == 2) {
-            return 5;
-        } else {
-            return 6;
-        }
+    private static int score(int count) {
+        if (count == 6) return 1;
+        if (count == 5) return 2;
+        if (count == 4) return 3;
+        if (count == 3) return 4;
+        if (count == 2) return 5;
+        else return 6;
     }
 }
