d69e90fbb12add0b12aabc003af69cc44ebf1019
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-04-03 18:40:45 +0900

    [level 1] Title: 덧칠하기, Time: 2.14 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -2,12 +2,14 @@ class Solution {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
         int lastPainted = 0;
+        
         for (int s : section) {
             if (s > lastPainted) {
                 answer++;
                 lastPainted = s + m - 1;
             }
         }
+
         return answer;
     }
 }
