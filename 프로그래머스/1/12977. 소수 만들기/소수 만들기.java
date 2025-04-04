753485aa556f44c9d26e9a57b1933b2221e25880
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-04-04 19:08:53 +0900

    [level 1] Title: 소수 만들기, Time: 2.72 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -5,23 +5,24 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i+1; j < nums.length; j++) {
                 for (int k = j+1; k < nums.length; k++) {
-                    int sum = nums[i] + nums[j] + nums[k];
-                    if (IsPrime(sum)) {
+                    if (isPrime(nums[i] + nums[j] + nums[k])) {
                         answer++;
                     }
                 }
             }
         }
+
         return answer;
     }
     
-    public static boolean IsPrime(int n) {
+    private static boolean isPrime(int n) {
         if (n < 2) {
             return false;
         }
         for (int i = 2; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
-                return false;
+                return false; // 소수는 1과 자기 자신으로만 나누어 떨어질 수 있으니까
+                // 만약 다른 수로 나누어 떨어지면 소수가 아님
             }
         }
         return true;
