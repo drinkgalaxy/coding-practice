f49a22575826fbde0958e68bd9f83d782280f9cb
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-04-05 23:47:05 +0900

    [Silver II] Title: 쇠막대기, Time: 156 ms, Memory: 16420 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    String iron = br.readLine();
+
+    Stack<Character> stack = new Stack<>();
+
+    int count = 0;
+
+    for (int i = 0; i < iron.length(); i++) {
+      if (iron.charAt(i) == '(') { // ( 일 경우
+        stack.push(iron.charAt(i));
+      } else { // ) 일 경우
+        stack.pop();
+        if (iron.charAt(i - 1) == '(') { // () 가 될 경우 레이저
+          count += stack.size();
+        } else { // )) 가 될 경우 마지막 조각을 합친 후 쇠막대기 종료
+          count++;
+        }
+      }
+    }
+
+    System.out.println(count);
+  }
+}
