1e8294f5b40839454d2e554c7921dd14492f03e4
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-04-06 21:11:15 +0900

    [Silver IV] Title: 수 찾기, Time: 520 ms, Memory: 43252 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,55 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
-
 
 public class Main {
   static int[] arr;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringBuilder sb = new StringBuilder();
 
     int N = Integer.parseInt(br.readLine());
-    arr = new int[N];
 
     StringTokenizer st = new StringTokenizer(br.readLine());
+    arr = new int[N];
     for (int i = 0; i < N; i++) {
       arr[i] = Integer.parseInt(st.nextToken());
     }
 
     Arrays.sort(arr);
 
+    StringBuilder sb = new StringBuilder();
+
     int M = Integer.parseInt(br.readLine());
-    int[] mrr = new int[M];
 
     st = new StringTokenizer(br.readLine());
     for (int i = 0; i < M; i++) {
-      mrr[i] = Integer.parseInt(st.nextToken());
-      if (binarySearch(mrr[i])) {
-        sb.append(1).append("\n");
-      } else {
-        sb.append(0).append("\n");
-      }
+      int num = Integer.parseInt(st.nextToken());
+      sb.append(isExist(num) ? 1 : 0).append("\n");
     }
 
     System.out.println(sb);
   }
 
-  public static boolean binarySearch(int target) {
-    int lo = 0;
-    int hi = arr.length-1;
-    while (lo <= hi) {
-      int mid = (lo + hi) / 2;
-      if (arr[mid] < target) {
-        lo = mid + 1;
-      } else if (arr[mid] > target) {
-        hi = mid - 1;
+  private static boolean isExist(int target) {
+    int s = 0;
+    int e = arr.length - 1;
+    while (s <= e) {
+
+      int mid = (s + e) / 2;
+
+      if (target < arr[mid]) {
+        e = mid - 1;
+      } else if (target > arr[mid]) {
+        s = mid + 1;
       } else {
         return true;
       }
