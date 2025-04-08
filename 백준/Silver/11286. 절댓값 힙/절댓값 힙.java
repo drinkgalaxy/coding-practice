f9352a031613c472ebdf59816215ba10d05d008c
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-04-08 09:58:19 +0900

    [Silver I] Title: 절댓값 힙, Time: 464 ms, Memory: 28044 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,47 +1,51 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PriorityQueue<ABS> queue = new PriorityQueue<>();
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine()); // 연산의 개수
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(br.readLine());
-            if (num == 0) {
-                if (!queue.isEmpty()) {
-                    sb.append(queue.poll().toString()).append("\n");
-                } else {
-                    sb.append(0).append("\n");
-                }
-            } else {
-                queue.offer(new ABS(num));
-            }
-        }
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println(sb);
+    int N = Integer.parseInt(br.readLine());
+
+    PriorityQueue<Class> queue = new PriorityQueue<>();
+
+    for (int i = 0; i < N; i++) {
+      int x = Integer.parseInt(br.readLine());
+      if (x != 0) {
+        queue.offer(new Class(x));
+      } else {
+        if (queue.isEmpty()) {
+          System.out.println(0);
+        } else {
+          System.out.println(queue.poll().toString());
+        }
+      }
+    }
+  }
+
+  private static class Class implements Comparable<Class> {
+    int x;
+    int lxl;
+
+    public Class(int x) {
+      this.x = x;
+      this.lxl = Math.abs(x);
     }
 
-    static public class ABS implements Comparable<ABS> {
-        int num;
-
-        public ABS(int num) {
-            this.num = num;
-        }
-
-        @Override
-        public int compareTo(ABS abs) {
-            if (Math.abs(this.num) == Math.abs(abs.num)) {
-                return this.num - abs.num;
-            } else {
-                return Math.abs(this.num) - Math.abs(abs.num);
-            }
-        }
-
-        @Override
-        public String toString() {
-            return num+"";
-        }
+    @Override
+    public int compareTo(Class c) {
+      if (this.lxl == c.lxl) {
+        return this.x - c.x;
+      } else {
+        return this.lxl - c.lxl;
+      }
     }
+
+    @Override
+    public String toString() {
+      return x+"";
+    }
+  }
 }
