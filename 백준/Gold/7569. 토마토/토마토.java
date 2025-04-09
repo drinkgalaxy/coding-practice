d918bbc78d30b417382030315d65ebd6d55a49c2
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-04-09 15:47:37 +0900

    [Gold V] Title: 토마토, Time: 616 ms, Memory: 126256 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -1,85 +1,92 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][][] tomato;
-    static int[] dx = {-1, 1, 0, 0, 0, 0}; // 좌, 우, 상, 하, 앞, 뒤
-    static int[] dy = {0, 0, -1, 1, 0, 0};
-    static int[] dh = {0, 0, 0, 0, 1, -1};
-    static int M, N, H;
-    static Queue<Node> queue = new LinkedList<>();
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        M = Integer.parseInt(st.nextToken()); // 가로칸의 수 = col
-        N = Integer.parseInt(st.nextToken()); // 세로칸의 수 = row
-        H = Integer.parseInt(st.nextToken()); // 쌓아올려지는 높이
+  static int[] dh = {1, -1, 0, 0, 0, 0};
+  static int[] dx = {0, 0, -1, 1, 0, 0};
+  static int[] dy = {0, 0, 0, 0, 1, -1};
+  static int[][][] box;
+  static Queue<Node> queue = new LinkedList<>();
+  static int H, N, M;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-        tomato = new int[H][N][M];
+    M = Integer.parseInt(st.nextToken());
+    N = Integer.parseInt(st.nextToken());
+    H = Integer.parseInt(st.nextToken());
 
-        for (int k = 0; k < H; k++) {
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine(), " ");
-                for (int j = 0; j < M; j++) {
-                    tomato[k][i][j] = Integer.parseInt(st.nextToken());
-                    if (tomato[k][i][j] == 1) {
-                        queue.offer(new Node(k, i, j, 0));
-                    }
-                }
-            }
+    box = new int[H][N][M];
+
+    for (int i = 0; i < H; i++) {
+      for (int j = 0; j < N; j++) {
+        st = new StringTokenizer(br.readLine());
+        for (int k = 0; k < M; k++) {
+          box[i][j][k] = Integer.parseInt(st.nextToken());
+          if (box[i][j][k] == 1) {
+            queue.offer(new Node(i, j, k, 0));
+          }
         }
-
-        BFS();
+      }
     }
 
-    public static void BFS() {
-        int date = 0;
-        while (!queue.isEmpty()) {
-            Node n = queue.poll();
-            date = n.date;
-            for (int i = 0; i < 6; i++) {
-                int h = dh[i] + n.h;
-                int x = dx[i] + n.x;
-                int y = dy[i] + n.y;
-                if (x < N && x >= 0 && y < M && y >= 0 && h < H && h >= 0) {
-                    if (tomato[h][x][y] == 0) {
-                        tomato[h][x][y] = 1;
-                        queue.offer(new Node(h, x, y, date + 1));
-                    }
-                }
-            }
-        }
-        if (check()) {
-            System.out.println(date);
-        } else {
-            System.out.println(-1);
-        }
+    bfs();
 
+  }
+
+  private static void bfs() {
+    int date = 0;
+    while (!queue.isEmpty()) {
+      Node now = queue.poll();
+      date = now.date;
+      for (int i = 0; i < 6; i++) {
+        int nh = now.h + dh[i];
+        int nx = now.x + dx[i];
+        int ny = now.y + dy[i];
+        if (nh < H && nx < N && ny < M && nh >= 0 && nx >= 0 && ny >= 0) {
+          if (box[nh][nx][ny] == 0) {
+            box[nh][nx][ny] = 1;
+            queue.offer(new Node(nh, nx, ny, date + 1));
+          }
+        }
+      }
     }
 
-    private static boolean check() {
-        for (int k = 0; k < H; k++) {
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (tomato[k][i][j] == 0) {
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
+    if (check()) {
+      System.out.println(date);
+    } else {
+      System.out.println(-1);
     }
+  }
 
-    public static class Node {
-        int h, x, y, date;
-
-        public Node(int h, int x, int y, int date) {
-            this.h = h;
-            this.x = x;
-            this.y = y;
-            this.date = date;
+  private static boolean check() {
+    for (int i = 0; i < H; i++) {
+      for (int j = 0; j < N; j++) {
+        for (int k = 0; k < M; k++) {
+          if (box[i][j][k] == 0) {
+            return false;
+          }
         }
+      }
     }
+    return true;
+  }
+
+  private static class Node {
+    int h;
+    int x;
+    int y;
+    int date;
+
+    public Node(int h, int x, int y, int date) {
+      this.h = h;
+      this.x = x;
+      this.y = y;
+      this.date = date;
+    }
+  }
 }
