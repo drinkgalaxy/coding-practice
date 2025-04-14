a9c4644fda04410409a07b6e763e7467fd21c1d5
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-04-14 18:06:05 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 280 ms, Memory: 44928 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -1,39 +1,38 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 
 public class Main {
-    public static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()); // 첫 번째 장대에 쌓인 원판의 개수
+  static StringBuilder sb = new StringBuilder();
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        sb.append((int) (Math.pow(2, N) - 1)).append("\n");
-        Hanoi(N, 1, 2, 3);
-        System.out.println(sb);
+    int N = Integer.parseInt(br.readLine());
+
+    sb.append((int)(Math.pow(2, N) - 1)).append("\n");
+    // 하노이 탑의 이동 횟수는 정확하게 2^N - 1 번
+    hanoi(N, 1, 2, 3);
+    System.out.println(sb);
+
+  }
+
+  private static void hanoi(int n, int from, int mid, int to) {
+    if (n == 1) {
+      sb.append(from).append(" ").append(to).append("\n");
+      return;
     }
 
-    /*
-     * N : 원판의 개수
-     * start : 출발지
-     * mid : 옮기기 위해 이동해야 하는 장소
-     * to : 목적지
-     */
-    public static void Hanoi(int N, int start, int mid, int to) {
-        // 이동할 원반의 수가 1개
-        if (N == 1) {
-            sb.append(start + " " + to + "\n");
-            return;
-        }
+    // n - 1 개의 원판이 첫 번째 장대에서 두 번째 장대로 가야 함
+    hanoi(n - 1, from, to, mid);
 
-        // A -> C로 옮겨야 할 때
+    // 그 다음 가장 큰 원판이 세 번째 장대로 이동하는 횟수는 1회
+    sb.append(from).append(" ").append(to).append("\n");
 
-        // STEP 1) N-1개를 A에서 B로 이동 (start -> mid)
-        Hanoi(N - 1, start, to, mid);
+    // 마지막으로 두 번째 장대에 있는 n - 1 개의 원판이 세 번째 장대로 가야 함
+    hanoi(n - 1, mid, from, to);
 
-        // STEP 2) 1개를 A에서 C로 이동 (start -> to)
-        sb.append(start + " " + to + "\n");
+  }
 
-        // STEP 3) N-1개를 B에서 C로 이동 (mid -> to)
-        Hanoi(N - 1, mid, start, to);
-    }
 }
