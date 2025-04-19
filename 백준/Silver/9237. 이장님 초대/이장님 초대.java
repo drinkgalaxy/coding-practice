7a4c46ee01674b8cd894cdce172afbb9a9aca929
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-04-19 17:44:33 +0900

    [Silver V] Title: 이장님 초대, Time: 364 ms, Memory: 29580 KB -BaekjoonHub

---
+++ 백준/Silver/9237. 이장님 초대/이장님 초대.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+
+    Integer[] arr = new Integer[N];
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+
+    Arrays.sort(arr, Collections.reverseOrder());
+
+    int max = 0;
+    for (int i = 0; i < N; i++) {
+      max = Math.max(max, arr[i] + i +1);
+    }
+
+    System.out.println(max + 1);
+  }
+}
