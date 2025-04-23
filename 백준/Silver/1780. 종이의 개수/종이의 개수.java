2b1fe3a5670c03a2409a36a9b638be0db002936a
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-04-23 15:23:17 +0900

    [Silver II] Title: 종이의 개수, Time: 812 ms, Memory: 319652 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.java
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -1,68 +1,71 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static StringBuilder sb = new StringBuilder();
-    static int[][] paper;
-    static int m1, z, p1;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static int[][] paper;
+  static int N, m1, z, p1;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        paper = new int[N][N];
+    N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < N; j++) {
-                paper[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
+    paper = new int[N][N];
 
-        quad(0, 0, N);
+    for (int i = 0; i < N; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < N; j++) {
+        paper[i][j] = Integer.parseInt(st.nextToken());
+      }
+    }
+    // 탐색 시작하기
+    quad(0, 0, N);
 
-        System.out.println(m1);
-        System.out.println(z);
-        System.out.println(p1);
+    System.out.println(m1);
+    System.out.println(z);
+    System.out.println(p1);
 
+  }
+  private static void quad(int x, int y, int size) {
+    // 종이가 모두 같은 수로 되어 있는지 확인
+    if (isAllSame(paper[x][y], x, y, size)) {
+      if (paper[x][y] == -1) {
+        m1++;
+      } else if (paper[x][y] == 0) {
+        z++;
+      } else {
+        p1++;
+      }
+      return;
     }
 
-    static void quad(int x, int y, int size) {
-        if (IsPossible(x, y, size, paper[x][y])) {
-            if (paper[x][y] == -1) {
-                m1++;
-            } else if (paper[x][y] == 0) {
-                z++;
-            } else {
-                p1++;
-            }
-            return;
+    int newSize = size / 3;
+
+    // 아니면 9개로 자르기
+
+    quad(x, y, newSize);
+    quad(x, y + newSize, newSize);
+    quad(x, y + 2 * newSize, newSize);
+
+    quad(x + newSize, y, newSize);
+    quad(x + newSize, y + newSize, newSize);
+    quad(x + newSize, y + 2 * newSize, newSize);
+
+    quad(x + 2 * newSize, y, newSize);
+    quad(x + 2 * newSize, y + newSize, newSize);
+    quad(x + 2 * newSize, y + 2 * newSize, newSize);
+
+  }
+
+  private static boolean isAllSame(int val, int x, int y, int size) {
+    for (int i = x; i < x + size; i++) {
+      for (int j = y; j < y + size; j++) {
+        if (paper[i][j] != val) {
+          return false;
         }
-
-        int newSize = size / 3;
-
-        // 9개의 사분면을 전부 해줘야 됨.
-        // 위에서부터 col 쪽으로 순서대로 호출한 것.
-        quad(x, y, newSize);
-        quad(x, y + newSize, newSize);
-        quad(x, y + 2 * newSize, newSize);
-
-        quad(x + newSize, y, newSize);
-        quad(x + newSize, y + newSize, newSize);
-        quad(x + newSize, y + 2 * newSize, newSize);
-
-        quad(x + 2 * newSize, y, newSize);
-        quad(x + 2 * newSize, y + newSize, newSize);
-        quad(x + 2 * newSize, y + 2 * newSize, newSize);
+      }
     }
-
-    static boolean IsPossible(int x, int y, int size, int val) {
-        for (int i = x; i < x + size; i++) {
-            for (int j = y; j < y + size; j++) {
-                if (paper[i][j] != val) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+    return true;
+  }
 }
