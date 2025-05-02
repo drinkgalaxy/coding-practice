6ac4d89ee91a3824d9a6bb6456ff9780a74f50f7
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-05-02 23:54:48 +0900

    [Silver III] Title: 계단 오르기, Time: 68 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,42 +1,36 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    static int[] arr;
-    static Integer[] dp;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+  static int[] arr;
+  static Integer[] dp;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        arr = new int[N + 1];
-        dp = new Integer[N + 1];
+    int N = Integer.parseInt(br.readLine()); // 계단의 개수
 
-        // 데이터 저장
-        for (int i = 1; i <= N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
+    arr = new int[N + 1];
+    dp = new Integer[N+1];
 
-        // 초기화
-        // index 0은 시작점
-        dp[0] = arr[0]; // 0으로 초기화
-        dp[1] = arr[1];
-
-        // N이 1일 경우를 대비해서 2 이상일 때만 dp[2]를 초기화
-        if (N >= 2) {
-            dp[2] = arr[1] + arr[2];
-        }
-
-        System.out.println(getMax(N));
+    for (int i = 1; i <= N; i++) {
+      arr[i] = Integer.parseInt(br.readLine());
     }
 
-    public static int getMax(int N) {
-
-        // 탐색하지 않은 값에 메모이제이션 저장
-        if (dp[N] == null) {
-            dp[N] = Math.max(getMax(N - 2), getMax(N - 3) + arr[N - 1]) + arr[N];
-        }
-
-        // 리턴
-        return dp[N];
+    dp[0] = arr[0]; // index 0 은 시작점
+    dp[1] = arr[1];
+    if (N >= 2) {
+      dp[2] = arr[1] + arr[2];
     }
 
+    System.out.println(getMax(N));
+  }
+
+  private static int getMax(int n) {
+    if (dp[n] == null) {
+      dp[n] = Math.max(getMax(n-3) + arr[n-1], getMax(n-2)) + arr[n];
+      // dp[4] = Math.max(getMax(1) + arr[3], getMax(2)) + arr[4];
+    }
+    return dp[n];
+  }
 }
