d60214771eb9c632bacb06899f216bb35e03d37c
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-05-08 21:44:27 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 508 ms, Memory: 56672 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,26 +1,36 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); // 데이터의 개수 N
-        int M = Integer.parseInt(st.nextToken()); // 출력할 개수 M
-        long[] sums = new long[N+1];
+    int N = Integer.parseInt(st.nextToken()); // 수의 개수
+    int M = Integer.parseInt(st.nextToken()); // 합을 구해야 하는 횟수
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= N; i++) {
-            sums[i] = sums[i-1] + Integer.parseInt(st.nextToken()); // N개의 수 입력받기
-        }
+    int[] arr = new int[N+1];
+    int[] sum = new int[N+1];
 
-        for (int k = 0; k < M; k++) {
-            st = new StringTokenizer(br.readLine());
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-
-            System.out.println(sums[j] - sums[i-1]);
-        }
+    st = new StringTokenizer(br.readLine());
+    for (int i = 1; i <= N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+      sum[i] = sum[i - 1] + arr[i];
     }
+
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < M; i++) {
+      st = new StringTokenizer(br.readLine());
+      int s = Integer.parseInt(st.nextToken());
+      int e = Integer.parseInt(st.nextToken());
+
+      sb.append(sum[e] - sum[s - 1]).append("\n");
+    }
+
+    System.out.println(sb);
+
+  }
 }
