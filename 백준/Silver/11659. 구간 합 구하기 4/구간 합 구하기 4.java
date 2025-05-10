c00eaf5a03dd62c9c4b9e79d9095f55a3771e5b0
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-05-10 20:00:13 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 508 ms, Memory: 56376 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -12,25 +12,20 @@ public class Main {
     int M = Integer.parseInt(st.nextToken()); // 합을 구해야 하는 횟수
 
     int[] arr = new int[N+1];
-    int[] sum = new int[N+1];
 
     st = new StringTokenizer(br.readLine());
     for (int i = 1; i <= N; i++) {
-      arr[i] = Integer.parseInt(st.nextToken());
-      sum[i] = sum[i - 1] + arr[i];
+      arr[i] = arr[i-1] + Integer.parseInt(st.nextToken()); // 누적합 저장
     }
 
     StringBuilder sb = new StringBuilder();
-
     for (int i = 0; i < M; i++) {
       st = new StringTokenizer(br.readLine());
       int s = Integer.parseInt(st.nextToken());
       int e = Integer.parseInt(st.nextToken());
-
-      sb.append(sum[e] - sum[s - 1]).append("\n");
+      sb.append(arr[e] - arr[s - 1]).append("\n");
     }
 
     System.out.println(sb);
-
   }
 }
