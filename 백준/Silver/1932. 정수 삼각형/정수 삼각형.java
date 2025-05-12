a6ebc426af1d13be323241b4e3a7f1e662ceb42f
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-05-13 00:57:40 +0900

    [Silver I] Title: 정수 삼각형, Time: 184 ms, Memory: 24420 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,42 +1,38 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+
 public class Main {
-    static int[][] arr;
-    static Integer[][] dp;
-    static int n;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static int[][] arr;
+  static int[][] dp;
 
-        n = Integer.parseInt(br.readLine()); // 삼각형의 크기
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        arr = new int[n][n];
-        dp = new Integer[n][n];
+    int n = Integer.parseInt(br.readLine()); // 삼각형의 크기
+    arr = new int[n][n];
+    dp = new int[n][n];
 
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < i + 1; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        // 초기화 -> 삼각형 맨 밑의 값으로 초기화
-        for (int i = 0; i < n; i++) {
-            dp[n - 1][i] = arr[n - 1][i];
-        }
-        System.out.println(getMax(0, 0));
+    for (int i = 0; i < n; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      for (int j = 0; j <= i; j++) {
+        arr[i][j] = Integer.parseInt(st.nextToken());
+      }
     }
 
-    private static int getMax(int depth, int index) {
-        // 초기화된 맨 밑의 값이면
-        if (depth == n - 1) {
-            return dp[n - 1][index];
-        }
-
-        if (dp[depth][index] == null) {
-            dp[depth][index] = Math.max(getMax(depth + 1, index) + arr[depth][index], getMax(depth + 1, index + 1) + arr[depth][index]);
-        }
-
-        return dp[depth][index];
+    for (int i = 0; i < n; i++) {
+      dp[n-1][i] = arr[n-1][i]; // 맨 아래 dp 초기값
     }
+
+    // 아래에서부터 위로 올라가면서 누적 최대값 계산
+    for (int i = n-2; i >= 0; i--) {
+      for (int j = 0; j <= i; j++) {
+        dp[i][j] = Math.max(dp[i+1][j], dp[i+1][j+1]) + arr[i][j];
+      }
+    }
+
+    System.out.println(dp[0][0]); // 최종 결과는 꼭대기
+  }
 }
