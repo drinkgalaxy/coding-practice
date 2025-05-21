d1395918c762a7e3b85494550a7cd4fa9e0be671
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-05-21 17:29:27 +0900

    [Silver III] Title: 이친수, Time: 68 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.java
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -1,20 +1,28 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        long [][] array = new long[N+1][2];
-        array[1][1] = 1; // 1자리 이친수는 단 1가지
-        array[1][0] = 0;
-        for (int i = 2; i <= N; i++) {
-            // i번째 0으로 끝나는 개수
-            array[i][0] = array[i-1][1] + array[i-1][0]; // 0은 이전단계의 0과 1로 끝나는 모든 경우에 붙일 수 있음
-            // i번째 1로 끝나는 개수
-            array[i][1] = array[i-1][0]; // 1은 이전 단계의 0으로 끝나는 모든 경우만 붙일 수 있음
-        }
-        System.out.println(array[N][0] + array[N][1]);
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    int N = Integer.parseInt(br.readLine());
+    long[][] dp = new long[N+1][2]; // 끝자리가 0 또는 1
+
+    // 1 자리 이친수 : 1
+    dp[1][1] = 1;
+    dp[1][0] = 0;
+    // 2 자리 이친수 : 10
+    // 3 자리 이친수 : 100, 101
+    // 4 자리 이친수 : 1000, 1001, 1010
+    // 5 자리 이친수 : 10000, 10001, 10100, 10101
+
+    for (int i = 2; i <= N; i++) {
+      dp[i][0] = dp[i-1][1] + dp[i-1][0]; // 0은 1, 0 뒤에 모두 붙일 수 있음
+      dp[i][1] = dp[i-1][0]; // 1은 0 뒤에만 붙일 수 있음
     }
-}
+    System.out.println(dp[N][0] + dp[N][1]);
 
+  }
+}
