e518eee8c948d6b55c39af00f9b438c8e3a6d5b2
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-05-22 17:29:44 +0900

    [Silver II] Title: 연속합, Time: 220 ms, Memory: 38044 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,42 +1,41 @@
-import java.io.*;
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static Integer[] dp; // 메모이제이션 dp
-    static int[] arr; // 배열
-    static int value = 0;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static int[] arr;
+  static Integer[] dp;
+  static int value;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+    int n = Integer.parseInt(br.readLine());
 
-        arr = new int[n];
-        dp = new Integer[n];
+    arr = new int[n+1];
+    dp = new Integer[n+1];
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        dp[0] = arr[0]; // 첫 원소 초기화
-        value = arr[0]; // 첫 원소 초기화
-
-        // dp의 마지막 index 는 n - 1
-        calculate(n - 1);
-
-        System.out.println(value);
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    for (int i = 1; i <= n; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
     }
 
-    public static int calculate(int n) {
-        // 탐색하지 않은 인덱스라면
-        if (dp[n] == null) {
-            // (이전 배열의 누적합 + 현재 값) 과 현재 값을 비교하여 최댓값을 저장
-            dp[n] = Math.max(calculate(n - 1) + arr[n], arr[n]);
-            value = Math.max(dp[n], value);
-        }
+   // 첫 원소 초기화
+    dp[0] = arr[0];
+    value = dp[1] = arr[1];
 
-        return dp[n];
+    getMax(n);
+
+    System.out.println(value);
+
+  }
+
+  private static int getMax(int n) {
+    if (dp[n] == null) {
+      // 이전 배열의 누적합 + 현재 값과 현재 값을 비교하여 최댓값 저장
+      dp[n] = Math.max(getMax(n-1) + arr[n], arr[n]);
+      value = Math.max(dp[n], value);
     }
+    return dp[n];
+  }
 }
