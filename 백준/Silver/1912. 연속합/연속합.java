cec50073a596048a3f0125ce1e52c16c34864da3
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-05-24 12:49:29 +0900

    [Silver II] Title: 연속합, Time: 168 ms, Memory: 22056 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -6,35 +6,36 @@ import java.util.StringTokenizer;
 public class Main {
   static int[] arr;
   static Integer[] dp;
-  static int value;
+  static int result;
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     int n = Integer.parseInt(br.readLine());
-
     arr = new int[n+1];
     dp = new Integer[n+1];
 
-    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    // 연속된 몇 개의 수를 선택
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
     for (int i = 1; i <= n; i++) {
       arr[i] = Integer.parseInt(st.nextToken());
     }
 
-   // 첫 원소 초기화
     dp[0] = arr[0];
-    value = dp[1] = arr[1];
+    result = dp[1] = arr[1];
 
-    getMax(n);
+    for (int i = 1; i <= n; i++) {
+      getResult(i);
+    }
 
-    System.out.println(value);
-
+    System.out.println(result);
   }
 
-  private static int getMax(int n) {
+  private static int getResult(int n) {
     if (dp[n] == null) {
-      // 이전 배열의 누적합 + 현재 값과 현재 값을 비교하여 최댓값 저장
-      dp[n] = Math.max(getMax(n-1) + arr[n], arr[n]);
-      value = Math.max(dp[n], value);
+      dp[n] = Math.max(getResult(n-1) + arr[n], arr[n]);
+      result = Math.max(dp[n], result);
     }
     return dp[n];
   }
