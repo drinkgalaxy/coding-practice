47f95fceff0461302acd5e897cdd59f8cd66a65f
Lee HyunJin <138424719+drinkgalaxy@users.noreply.github.com>
2025-05-27 10:39:05 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 84 ms, Memory: 12304 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,44 +1,48 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static Integer[] dp;
-    static int[] arr;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static Integer[] dp;
+  static int[] arr;
 
-        int N = Integer.parseInt(br.readLine()); // 수열 A의 크기
-        dp = new Integer[N];
-        arr = new int[N];
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+    int N = Integer.parseInt(br.readLine());
+    dp = new Integer[N];
+    arr = new int[N];
 
-        for (int i = 0; i < N; i++) {
-            LIS(i);
-        }
-
-        int max = -1;
-        for (int i = 0; i < N; i++) {
-            max = Math.max(dp[i], max);
-        }
-
-        System.out.println(max);
-
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
     }
 
-    private static int LIS(int n) {
-        if (dp[n] == null) {
-            dp[n] = 1;
-
-            for (int i = n-1; i >= 0; i--) {
-                if (arr[i] < arr[n]) {
-                    dp[n] = Math.max(dp[n], LIS(i) + 1);
-                }
-            }
-        }
-        return dp[n];
+    // 부분 증가 수열 호출
+    for (int i = 0; i < N; i++) {
+      process(i);
     }
+
+    // 최댓값 길이 찾기
+    int max = dp[0];
+    for (int i = 1; i < N; i++) {
+      max = Math.max(max, dp[i]);
+    }
+
+    System.out.println(max);
+
+  }
+
+  private static int process(int n) {
+    if (dp[n] == null) {
+      dp[n] = 1;
+      for (int i = 0; i < n; i++) {
+        if (arr[i] < arr[n]) {
+          dp[n] = Math.max(dp[n], process(i) + 1);
+        }
+      }
+    }
+    return dp[n];
+  }
 }
