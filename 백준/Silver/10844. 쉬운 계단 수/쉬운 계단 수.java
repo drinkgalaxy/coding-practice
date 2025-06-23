c0d29431e4064bef3975995d9f52ac03dc5b5c0e
Hyunjin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2025-06-23 20:11:10 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 60 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,44 +1,42 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static Long[][] dp;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        dp = new Long[N + 1][10]; // N개의 자릿값을 표현하고, 그 자릿값은 각각 0~9의 값을 가짐
+    int N = Integer.parseInt(br.readLine());
 
-        // 첫 번째 자리수 1로 초기화
-        for (int i = 0; i < 10; i++) {
-            dp[1][i] = 1L;
-        }
+    int[][] dp = new int[N+1][10];
 
-        long result = 0;
+    // 자리수 길이와 마지막 숫자를 기준으로 계단 수를 셈
 
-        for (int i = 1; i <= 9; i++) {
-            result += dfs(N, i);
-        }
-        System.out.println(result % 1000000000);
-
-
+    for (int i = 1; i <= 9; i++) { // 다만, 0으로 시작하는 수는 계단 수가 아님.
+      dp[1][i] = 1; // 1자리 수 중 1~9는 계단 수
     }
 
-    public static Long dfs(int digit, int val) {
-        if (digit == 1) {
-            return dp[digit][val];
+    for (int i = 2; i <= N; i++) {
+      for (int j = 0; j <= 9; j++) {
+        if (j == 0) {
+          dp[i][j] += dp[i-1][1];
+        } else if (j == 9) {
+          dp[i][j] += dp[i-1][8];
+        } else {
+          dp[i][j] += (dp[i-1][j-1] + dp[i-1][j+1]);
         }
 
-        if (dp[digit][val] == null) {
-            if (val == 0) {
-                dp[digit][val] = dfs(digit - 1, 1);
-            }  else if (val == 9) {
-                dp[digit][val] = dfs(digit - 1, 8);
-            } else {
-                dp[digit][val] = dfs(digit - 1, val - 1) + dfs(digit - 1, val + 1);
-            }
-        }
-
-        return dp[digit][val] % 1000000000;
+        dp[i][j] %= 1000000000;
+      }
     }
 
+    // 길이가 N 인 계단 수의 합
+    int result = 0;
+    for (int i = 0; i <= 9; i++) {
+      result = (result + dp[N][i]) % 1000000000;
+    }
+
+    System.out.println(result);
+  }
 }
