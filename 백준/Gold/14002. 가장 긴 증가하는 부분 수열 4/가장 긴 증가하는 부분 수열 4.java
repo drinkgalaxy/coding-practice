1a7fc4d57df42681098b15a3d26b56d52b7d554b
Hyunjin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2025-07-11 23:56:18 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 88 ms, Memory: 12756 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+  static int[] arr;
+  static Integer[] dp;
+  static int[] pre;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+    arr = new int[N];
+    dp = new Integer[N];
+    pre = new int[N];
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+      pre[i] = -1; // 초기화
+    }
+
+    // 0부터 n-1까지 탐색 진행
+    for (int i = 0; i < N; i++) {
+      LIS(i);
+    }
+
+    int max = dp[0];
+    int maxIndex = 0;
+    for (int i = 0; i < dp.length; i++) {
+      if (max < dp[i]) {
+        max = dp[i];
+        maxIndex = i;
+      }
+    }
+
+    System.out.println(max);
+
+    // 역추적하면서 수열 출력
+    Stack<Integer> stack = new Stack<>();
+    while (maxIndex != -1) {
+      stack.push(arr[maxIndex]);
+      maxIndex = pre[maxIndex];
+    }
+
+    StringBuilder sb = new StringBuilder();
+    while (!stack.isEmpty()) {
+      sb.append(stack.pop()).append(" ");
+    }
+    System.out.println(sb);
+
+  }
+
+  private static int LIS(int n) {
+    if (dp[n] == null) {
+      dp[n] = 1;
+      for (int i = 0; i < n; i++) {
+        if (arr[i] < arr[n]) {
+          int lis = LIS(i) + 1;
+          if (dp[n] < lis) {
+            dp[n] = lis;
+            pre[n] = i; // 이전 인덱스 저장
+          }
+        }
+      }
+    }
+    return dp[n];
+  }
+}
