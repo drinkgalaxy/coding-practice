a122d0ff9c9aa04111f5dfa54ea51a60d1a4604e
Hyunjin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2025-07-15 17:58:06 +0900

    [Gold V] Title: 극장 좌석, Time: 64 ms, Memory: 11452 KB -BaekjoonHub

--- 백준/Gold/2302. 극장 좌석/극장 좌석.java
+++ 백준/Gold/2302. 극장 좌석/극장 좌석.java
@@ -7,33 +7,38 @@ public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    int N = Integer.parseInt(br.readLine()); // 좌석의 개수
-    int M = Integer.parseInt(br.readLine()); // 고정석의 개수
+    int N = Integer.parseInt(br.readLine());
+    int M = Integer.parseInt(br.readLine());
 
     int[] fix = new int[M+1];
     for (int i = 1; i <= M; i++) {
       fix[i] = Integer.parseInt(br.readLine());
     }
 
+    // N = 1 일 때, 1가지
+    // N = 2 일 때, 2가지
+    // N = 3 일 때, (123, 132, 213) 3가지
+    // N = 4 일 때, (1234, 2134, 1243, 2143, 1324) 5가지
+    // 따라서 점화식 dp[i] = dp[i-1] + dp[i-2]
+
     long[] dp = new long[N+1];
-    dp[0] = 1; // 공집합일 경우의 수는 1
+    dp[0] = 1; // 공집합은 1
     dp[1] = 1;
     if (N >= 2) {
-      dp[2] = 2;
+      dp[2] = dp[1] + dp[0];
     }
     for (int i = 3; i <= N; i++) {
       dp[i] = dp[i-1] + dp[i-2];
     }
 
-    // 고정석을 기준으로 나눈 덩어리를 구해 정답 구함
+    // 덩어리 구하기
     long answer = 1;
     for (int i = 1; i <= M; i++) {
-      int free = fix[i] - fix[i-1] - 1; // 사이에 있는 자유석의 개수
-      answer *= dp[free];
+      int seats = fix[i] - fix[i-1] - 1;
+      answer *= dp[seats];
     }
-    // 마지막 자유석의 개수
+    // 마지막 덩어리 구하기
     answer *= dp[N - fix[M]];
-
 
     System.out.println(answer);
   }
