771f33aa08c968260d6a0183c1a1c2bc5f068ef8
Hyunjin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2025-07-18 16:59:22 +0900

    [Gold IV] Title: 동전 1, Time: 76 ms, Memory: 11652 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int k = Integer.parseInt(st.nextToken());
+
+    int[] coins = new int[n];
+    for (int i = 0; i < n; i++) {
+      coins[i] = Integer.parseInt(br.readLine());
+    }
+
+    int[] dp = new int[k+1];
+    dp[0] = 1;
+    // dp[k] = 금액 k를 만드는 경우의 수
+
+    for (int coin : coins) {
+      for (int i = coin; i <= k; i++) {
+        dp[i] += dp[i - coin];
+      }
+    }
+
+    System.out.println(dp[k]);
+  }
+}
