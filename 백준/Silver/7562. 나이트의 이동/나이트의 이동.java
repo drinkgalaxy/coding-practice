4c535d5d636130b14541372b6afdd89d38fd6d2d
Hyunjin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2025-07-29 18:08:21 +0900

    [Silver I] Title: 나이트의 이동, Time: 208 ms, Memory: 62376 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,60 +1,78 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] dx = {-2, -1, 1, 2, 2, 1, -1, -2};
-    static int[] dy = {1, 2, 2, 1, -1, -2, -2, -1};
-    static int[][] dist;
-    static int sx, sy, ex, ey, L;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static int I;
+  static boolean[][] visited;
+  static Queue<Node> queue;
+  static int[] dx = {-2, -1, 1, 2, 2, 1, -1, -2};
+  static int[] dy = {1, 2, 2, 1, -1, -2, -2, -1};
+  static int result;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine()); // 테스트 케이스의 개수
+    StringBuilder sb = new StringBuilder();
+    int T = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < T; i++) {
-            L = Integer.parseInt(br.readLine()); // 체스판의 한 변의 길이
-            dist = new int[L][L];
+    for (int i = 0; i < T; i++) {
+      I = Integer.parseInt(br.readLine());
+      visited = new boolean[I][I];
+      queue = new LinkedList<>();
 
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            sx = Integer.parseInt(st.nextToken());
-            sy = Integer.parseInt(st.nextToken());
-            st = new StringTokenizer(br.readLine(), " ");
-            ex = Integer.parseInt(st.nextToken());
-            ey = Integer.parseInt(st.nextToken());
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int sx = Integer.parseInt(st.nextToken());
+      int sy = Integer.parseInt(st.nextToken());
 
-            System.out.println(BFS());
-        }
+      st = new StringTokenizer(br.readLine());
+      int ex = Integer.parseInt(st.nextToken());
+      int ey = Integer.parseInt(st.nextToken());
+
+      Node start = new Node(sx, sy, 0);
+
+      queue.add(start);
+      visited[start.x][start.y] = true;
+      Bfs(ex, ey);
+
+      sb.append(result).append("\n");
     }
+    System.out.println(sb);
+  }
 
-    private static int BFS() {
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{sx, sy});
-        dist[sx][sy] = 0; // 시작점은 거리가 0
+  private static void Bfs(int ex,  int ey) {
 
-        while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            int nx = now[0];
-            int ny = now[1];
+    while (!queue.isEmpty()) {
+      Node node = queue.poll();
 
-            // 도착지에 도착했을 경우
-            if (nx == ex && ny == ey) {
-                return dist[ex][ey];
-            }
+      if (node.x == ex && node.y == ey) {
+        result = node.count;
+        return;
+      }
 
-            for (int i = 0; i < 8; i++) {
-                int xx = nx + dx[i];
-                int yy = ny + dy[i];
-
-                if (xx < L && yy < L && xx >= 0 && yy >= 0) {
-                    if (dist[xx][yy] == 0) {
-                        queue.offer(new int[]{xx, yy});
-                        dist[xx][yy] += dist[nx][ny] + 1;
-                    }
-                }
-            }
+      for (int i = 0; i < 8; i++) {
+        int nx = dx[i] + node.x;
+        int ny = dy[i] + node.y;
+        if (nx < I && ny < I && nx >= 0 && ny >= 0) {
+          if (!visited[nx][ny]) {
+            queue.add(new Node(nx, ny, node.count + 1));
+            visited[nx][ny] = true;
+          }
         }
-        return -1;
+      }
     }
+  }
+
+  private static class Node {
+    int x, y;
+    int count;
+
+    public Node(int x, int y, int count) {
+      this.x = x;
+      this.y = y;
+      this.count = count;
+    }
+  }
 }
