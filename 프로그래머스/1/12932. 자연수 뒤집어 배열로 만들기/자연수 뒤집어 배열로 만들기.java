722bf3ee41e2b78727c4aacf644bb23f256c6690
HyunJin Lee <138424719+drinkgalaxy@users.noreply.github.com>
2026-06-08 21:25:09 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.46 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -1,13 +1,18 @@
+import java.util.*;
 class Solution {
     public int[] solution(long n) {
-        StringBuilder sb = new StringBuilder();
         
-        String value = sb.append(Long.toString(n)).reverse().toString();
+        Stack<Integer> stack = new Stack<>();
         
-        int[] answer = new int[value.length()];
-        int idx = 0;
-        for (String s : value.split("")) {
-            answer[idx++] = Integer.parseInt(s);
+        String sn = String.valueOf(n);
+        for (String s : sn.split("")) {
+            stack.push(Integer.parseInt(s));
+        }
+        
+        int[] answer = new int[stack.size()];
+        int index = 0;
+        while (!stack.isEmpty()) {
+            answer[index++] = stack.pop();
         }
         
         return answer;
